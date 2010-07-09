9446424c6d4fe16db2d6f32d84f59df5fb3490fc
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-07-09 11:56:58 +0000

    usecases
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11182 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-usecases/src/test/java/uk/org/taverna/scufl2/usecases/TestProcessorNames.java
@@ -0,0 +1,40 @@
+package uk.org.taverna.scufl2.usecases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Test;
+
+import uk.org.taverna.scufl2.api.container.TavernaResearchObject;
+import uk.org.taverna.scufl2.translator.t2flow.ParseException;
+import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
+
+public class TestProcessorNames {
+
+	@Test
+	public void processorNames() throws JAXBException, IOException,
+			ParseException {
+		InputStream workflow = getClass()
+		.getResourceAsStream(
+				"/workflows/biomartandembossanalysis_904962.t2flow");
+		assertNotNull(workflow);
+		
+		T2FlowParser t2flowParser = new T2FlowParser();
+		TavernaResearchObject ro = t2flowParser.parseT2Flow(workflow);
+		
+		List<String> expected = Arrays.asList("CreateFasta",
+				"FlattenImageList", "GetUniqueHomolog", "emma",
+				"getHSapSequence", "getMMusSequence", "getRNorSequence",
+				"hsapiensGeneEnsembl", "plot", "seqret");
+		ProcessorNames processorNames = new ProcessorNames();
+		assertEquals(expected, processorNames.showProcessorNames(ro));
+		System.out.println(processorNames.showProcessorTree(ro));
+	}
+}
