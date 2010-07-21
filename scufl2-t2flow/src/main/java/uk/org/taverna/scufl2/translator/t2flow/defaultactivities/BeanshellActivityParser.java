7f229b0a8f0996c2f2f232dbdeacc948301fdf6b
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-07-21 16:12:44 +0100

    parsers for all activities (just namespace mapping for now)

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BeanshellActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BeanshellActivityParser.java
@@ -9,7 +9,12 @@ public class BeanshellActivityParser implements T2Parser {
 	private static URI activityRavenURI = URI
 			.create("http://ns.taverna.org.uk/2010/activity/raven/net.sf.taverna.t2.activities/beanshell-activity/");
 
+	private static URI localWorkerActivityRavenURI = URI
+			.create("http://ns.taverna.org.uk/2010/activity/raven/net.sf.taverna.t2.activities/localworker-activity/");
+
 	private static String activityClassName = "net.sf.taverna.t2.activities.beanshell.BeanshellActivity";
+
+	private static String localWorkerActivityClassName = "net.sf.taverna.t2.activities.localworker.LocalworkerActivity";
 
 	public static URI scufl2Uri = URI
 			.create("http://ns.taverna.org.uk/2010/activity/beanshell");
@@ -17,8 +22,16 @@ public class BeanshellActivityParser implements T2Parser {
 	@Override
 	public boolean canHandlePlugin(URI activityURI) {
 		String activityUriStr = activityURI.toASCIIString();
-		return activityUriStr.startsWith(activityRavenURI.toASCIIString())
-				&& activityUriStr.endsWith(activityClassName);
+		if (activityUriStr.startsWith(activityRavenURI.toASCIIString())
+				&& activityUriStr.endsWith(activityClassName)) {
+			return true;
+		}
+		if (activityUriStr.startsWith(localWorkerActivityRavenURI
+				.toASCIIString())
+				&& activityUriStr.endsWith(localWorkerActivityClassName)) {
+			return true;
+		}
+		return false;
 	}
 
 	@Override
