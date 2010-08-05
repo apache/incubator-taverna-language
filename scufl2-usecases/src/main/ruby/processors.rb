336591dd0624a4bf92a84a8e78d96e51d32d7344
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-08-05 16:00:57 +0100

    witout puts

--- scufl2-usecases/src/main/ruby/processors.rb
+++ scufl2-usecases/src/main/ruby/processors.rb
@@ -9,29 +9,26 @@ dc = RDF::Vocabulary.new("http://purl.org/dc/elements/1.1/")
 graph = RDF::Graph.load("../resources/workflows/example.ttl")
 
 graph.query([nil, scufl2.workflow, nil]) do |bundle,p,workflow|
-  workflow_name = ""
   graph.query([workflow, scufl2.name, nil]) do |wf,p,workflow_name|
-    # Re-assign to workflow_name
+    # Should just be one
+    print workflow_name  
   end
-
-  description = ""
   graph.query([workflow, dc.description, nil]) do |workflow,p,description|
+    # Optional description(s)
+    print "  "
+    print description
   end
-
-  # FIXME: How do I get this on a single line?
-  puts workflow_name, description
+  print "\n"
 
   graph.query([workflow, scufl2.processor, nil]) do |workflow,p,processor|
-    processor_name = ""
-
     graph.query([processor, scufl2.name, nil]) do |processor,p,processor_name|
-        # This looks a bit silly - is there a better way to just extract
-        # from the first one?
+        print processor_name
     end
-
     graph.query([processor, dc.description, nil]) do |processor,p,description|
+        print "  "
+        print description
     end
-
-    puts processor_name, description
+    print "\n"
+    
   end
 end
