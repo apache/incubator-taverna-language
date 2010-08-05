74002d2087b47be511be1f6f56b00ddaab7e47f0
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-08-05 15:51:54 +0100

    yo

--- scufl2-usecases/src/main/ruby/processors.rb
+++ scufl2-usecases/src/main/ruby/processors.rb
@@ -2,26 +2,32 @@
 require 'rubygems'
 require 'rdf'
 require 'rdf/raptor'
-include RDF
 
 scufl2 = RDF::Vocabulary.new("http://ns.taverna.org.uk/2010/scufl2/ontology/")
+dc = RDF::Vocabulary.new("http://purl.org/dc/elements/1.1/")
 
 graph = RDF::Graph.load("../resources/workflows/example.ttl")
 
-graph.query([nil, scufl2.workflow, nil]) do |statement|
-  workflow = statement.object
-  graph.query([workflow, scufl2.name, nil]) do |statement|
-    workflow_name = statement.object
-    puts workflow_name
+graph.query([nil, scufl2.workflow, nil]) do |bundle,p,workflow|
+  workflow_name = ""
+  graph.query([workflow, scufl2.name, nil]) do |wf,p,workflow_name|
   end
-  graph.query([workflow, scufl2.processor, nil]) do |statement|
-    processor = statement.object
-    graph.query([statement.object, scufl2.name, nil]) do |statement|
-        processor_name = statement.object
-        puts processor_name
+
+  description = ""
+  graph.query([workflow, dc.description, nil]) do |workflow,p,description|
+  end
+
+  puts workflow_name, description
+
+  graph.query([workflow, scufl2.processor, nil]) do |workflow,p,processor|
+    processor_name = ""
+
+    graph.query([processor, scufl2.name, nil]) do |processor,p,processor_name|
     end
+
+    graph.query([processor, dc.description, nil]) do |processor,p,description|
+    end
+
+    puts processor_name, description
   end
-
-
-  
 end
