b74a00ab55ecfeb5569e8d3642719c87a8dc2f17
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-08-05 17:01:39 +0100

    now reading scufl2

--- scufl2-usecases/src/main/ruby/processors.rb
+++ scufl2-usecases/src/main/ruby/processors.rb
@@ -2,11 +2,48 @@
 require 'rubygems'
 require 'rdf'
 require 'rdf/raptor'
+require 'zip/zipfilesystem'
 
 scufl2 = RDF::Vocabulary.new("http://ns.taverna.org.uk/2010/scufl2/ontology/")
 dc = RDF::Vocabulary.new("http://purl.org/dc/elements/1.1/")
 
-graph = RDF::Graph.load("../resources/workflows/example.ttl")
+#graph = RDF::Graph.load("../resources/workflows/example.ttl")
+
+graph = RDF::Graph.new()
+Zip::ZipFile.open("../resources/workflows/example.scufl2") {
+    |zipfile|
+    a = "@base <http://example.org/> .\n" + zipfile.file.read("workflowBundle.ttl")
+    RDF::Reader.for(:turtle).new(a) do |reader|
+      reader.each_statement do |statement|
+        graph << statement
+      end
+    end
+
+    base = "http://example.org/" 
+    graph.query([nil,scufl2.sameBaseAs,nil]) do |s,p,base|
+    end 
+
+    a = "@base <" + base + "workflowBundle.ttl> .\n" + zipfile.file.read("workflowBundle.ttl")
+    RDF::Reader.for(:turtle).new(a) do |reader|
+      reader.each_statement do |statement|
+        graph << statement
+      end
+    end
+    # TODO: FOR-loop like in Python
+
+    a = "@base <" + base + "workflow/HelloWorld.ttl> .\n" + zipfile.file.read("workflow/HelloWorld.ttl")
+    RDF::Reader.for(:turtle).new(a) do |reader|
+      reader.each_statement do |statement|
+        graph << statement
+      end
+    end
+    a = "@base <" + base + "annotation/workflow/HelloWorld.ttl> .\n" + zipfile.file.read("annotation/workflow/HelloWorld.ttl")
+    RDF::Reader.for(:turtle).new(a) do |reader|
+      reader.each_statement do |statement|
+        graph << statement
+      end
+    end
+}
 
 graph.query([nil, scufl2.workflow, nil]) do |bundle,p,workflow|
   graph.query([workflow, scufl2.name, nil]) do |wf,p,workflow_name|
@@ -32,3 +69,5 @@ graph.query([nil, scufl2.workflow, nil]) do |bundle,p,workflow|
     
   end
 end
+
+
