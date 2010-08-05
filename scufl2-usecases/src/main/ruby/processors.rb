2718895a21f3f9286276a51be8fa4541aaeeecbe
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-08-05 15:53:05 +0100

    yo

--- scufl2-usecases/src/main/ruby/processors.rb
+++ scufl2-usecases/src/main/ruby/processors.rb
@@ -11,18 +11,22 @@ graph = RDF::Graph.load("../resources/workflows/example.ttl")
 graph.query([nil, scufl2.workflow, nil]) do |bundle,p,workflow|
   workflow_name = ""
   graph.query([workflow, scufl2.name, nil]) do |wf,p,workflow_name|
+    # Re-assign to workflow_name
   end
 
   description = ""
   graph.query([workflow, dc.description, nil]) do |workflow,p,description|
   end
 
+  # FIXME: How do I get this on a single line?
   puts workflow_name, description
 
   graph.query([workflow, scufl2.processor, nil]) do |workflow,p,processor|
     processor_name = ""
 
     graph.query([processor, scufl2.name, nil]) do |processor,p,processor_name|
+        # This looks a bit silly - is there a better way to just extract
+        # from the first one?
     end
 
     graph.query([processor, dc.description, nil]) do |processor,p,description|
