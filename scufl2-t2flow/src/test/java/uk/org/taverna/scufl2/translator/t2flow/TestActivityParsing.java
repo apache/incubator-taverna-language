bf419cff0731e49f94072d764d33c968bda1802a
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-09-07 09:46:02 +0000

    activity config testing
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11524 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/TestActivityParsing.java
@@ -0,0 +1,27 @@
+package uk.org.taverna.scufl2.translator.t2flow;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+
+import org.junit.Test;
+
+import uk.org.taverna.scufl2.api.container.TavernaResearchObject;
+import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
+
+public class TestActivityParsing {
+
+	private static final String WORKFLOW = "/defaultActivitiesTaverna2.2.t2flow";
+
+	@Test
+	public void readSimpleWorkflow() throws Exception {
+		URL wfResource = getClass().getResource(WORKFLOW);
+		assertNotNull("Could not find workflow " + WORKFLOW, wfResource);
+		T2FlowParser parser = new T2FlowParser();
+		parser.setStrict(true);
+		TavernaResearchObject researchObj = parser.parseT2Flow(wfResource.openStream());
+		System.out.println(researchObj);
+	}
+
+}
