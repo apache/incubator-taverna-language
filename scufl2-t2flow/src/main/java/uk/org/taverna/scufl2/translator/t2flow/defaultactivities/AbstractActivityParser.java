e025ba64cabf6d70a0cb85f90743c13f20ea075f
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-09-07 09:50:55 +0000

    common parser
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11542 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/AbstractActivityParser.java
@@ -0,0 +1,36 @@
+package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.w3c.dom.Element;
+
+import uk.org.taverna.scufl2.translator.t2flow.ParseException;
+import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
+import uk.org.taverna.scufl2.translator.t2flow.T2Parser;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
+
+public abstract class AbstractActivityParser implements T2Parser {
+
+	public <ConfigType> ConfigType unmarshallConfig(T2FlowParser t2FlowParser,
+			ConfigBean configBean, String encoding, Class<ConfigType> configType)
+			throws ParseException {
+		Object config = configBean.getAny();
+		if (!(config instanceof Element)
+				|| !configBean.getEncoding().equals(encoding)) {
+			throw new ParseException("Unsupported config bean " + configBean);
+		}
+		Unmarshaller unmarshaller2 = t2FlowParser.getUnmarshaller();
+		unmarshaller2.setSchema(null);
+		JAXBElement<ConfigType> configElemElem;
+		try {
+			configElemElem = unmarshaller2.unmarshal((Element) config,
+					configType);
+		} catch (JAXBException e) {
+			throw new ParseException("Can't parse config bean " + configBean, e);
+		}
+
+		return configElemElem.getValue();
+	}
+}
