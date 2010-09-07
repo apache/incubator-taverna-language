a31a77cb480be1fe5bbe5bedb8591cf99ad3b1f1
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-09-07 09:45:44 +0000

    Moving towards activity parsing
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11523 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/T2Parser.java
@@ -0,0 +1,11 @@
+package uk.org.taverna.scufl2.translator.t2flow;
+
+import java.net.URI;
+
+public interface T2Parser {
+
+	boolean canHandlePlugin(URI activityURI);
+
+	URI mapT2flowActivityToURI(URI t2flowActivity);
+
+}
