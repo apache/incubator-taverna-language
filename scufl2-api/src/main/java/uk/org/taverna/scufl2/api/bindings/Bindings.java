8574c8a4b2ed6a35bc2b52524b3e21022bfa663a
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-09-07 09:44:07 +0000

    named constructors
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11515 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/bindings/Bindings.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/bindings/Bindings.java
@@ -2,6 +2,7 @@ package uk.org.taverna.scufl2.api.bindings;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
@@ -28,6 +29,14 @@ import uk.org.taverna.scufl2.api.common.WorkflowBean;
 public class Bindings extends AbstractNamed implements WorkflowBean {
 
 	private Set<ProcessorBinding> processorBindings = new HashSet<ProcessorBinding>();
+
+	public Bindings() {
+		super();
+	}
+	
+	public Bindings(String name) {
+		super(name);
+	}
 
 	/**
 	 * Return the set of bindings for individual Processors.
