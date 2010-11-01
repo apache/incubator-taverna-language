242a47e55408e2594b2761d12731efdd2a279bc5
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-11-01 10:38:44 +0000

    String constant translation
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11791 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/StringConstantActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/StringConstantActivityParser.java
@@ -2,19 +2,26 @@ package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
 import java.net.URI;
 
+import org.junit.matchers.StringContains;
+
 import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.configurations.DataProperty;
+import uk.org.taverna.scufl2.api.configurations.ObjectProperty;
+import uk.org.taverna.scufl2.translator.t2flow.ParseException;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.translator.t2flow.T2Parser;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.StringConstantConfig;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.WSDLConfig;
 
-public class StringConstantActivityParser implements T2Parser {
+public class StringConstantActivityParser extends AbstractActivityParser {
 
 	private static URI activityRavenURI =
 			T2FlowParser.ravenURI.resolve("net.sf.taverna.t2.activities/stringconstant-activity/");
 
 	private static String activityClassName = "net.sf.taverna.t2.activities.stringconstant.StringConstantActivity";
 
-	public static URI scufl2Uri = URI
+	public static URI CONSTANT = URI
 			.create("http://ns.taverna.org.uk/2010/activity/constant");
 
 	@Override
@@ -26,14 +33,24 @@ public class StringConstantActivityParser implements T2Parser {
 
 	@Override
 	public URI mapT2flowActivityToURI(URI t2flowActivity) {
-		return scufl2Uri;
+		return CONSTANT;
 	}
 
 	@Override
 	public Configuration parseActivityConfiguration(T2FlowParser t2FlowParser,
-			ConfigBean configBean) {
-		// TODO Auto-generated method stub
-		return null;
+			ConfigBean configBean) throws ParseException {
+		StringConstantConfig strConfig = unmarshallConfig(t2FlowParser,
+				configBean, "xstream", StringConstantConfig.class);
+		String value = strConfig.getValue();
+		if (value == null) {
+			throw new ParseException("String constant configuration has no value set");
+		}
+		Configuration configuration = new Configuration();
+
+		configuration = new Configuration();
+		DataProperty prop = new DataProperty(CONSTANT.resolve("#string"), value);
+		configuration.getProperties().add(prop);
+		return configuration;
 	}
 
 
