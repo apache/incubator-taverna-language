cf22e3824a48c16401c136962fa902c976d10c11
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-11-09 14:03:02 +0000

    simple zip-file creation and testing
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@11874 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-bundle/src/main/java/uk/org/taverna/scufl2/bundle/Scufl2Bundle.java
+++ scufl2-bundle/src/main/java/uk/org/taverna/scufl2/bundle/Scufl2Bundle.java
@@ -1,29 +1,107 @@
 package uk.org.taverna.scufl2.bundle;
 
-import java.net.URI;
-import java.util.ArrayList;
-
-import org.odftoolkit.odfdom.pkg.OdfPackage;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CodingErrorAction;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Scufl2Bundle {
+
+	private static final String MIMETYPE = "mimetype";
+	public static final String MIME_SCUFL2_BUNDLE = "application/vnd.taverna.bundle";
+	public static final String MIME_WORKFLOW_BUNDLE = "application/vnd.taverna.workflow-bundle";
+	public static final String MIME_DATA_BUNDLE = "application/vnd.taverna.data-bundle";
+	public static final String MIME_WORKFLOW_RUN_BUNDLE = "application/vnd.taverna.workflow-run-bundle";
+	public static final String MIME_SERVICE_BUNDLE = "application/vnd.taverna.service-bundle";
+
+	private String mimeType = MIME_SCUFL2_BUNDLE;
+
+	private static Charset ASCII = Charset.forName("ascii");
+
 	public Scufl2Bundle() throws Exception {
 
-		OdfPackage odfPackage = OdfPackage.loadPackage("quick.odt");
-		System.out.println(odfPackage.getFileEntries());
+		// OdfPackage odfPackage = OdfPackage.loadPackage("quick.odt");
+		// System.out.println(odfPackage.getFileEntries());
+		//
+		// for (String odfFileEntry : new ArrayList<String>(
+		// odfPackage.getFileEntries())) {
+		// if (odfFileEntry.equals("/")) {
+		// continue;
+		// }
+		// odfPackage.remove(odfFileEntry);
+		// }
+		//
+		// odfPackage.insert(URI.create("foo.xml"), "workflows/deadbeef.xml",
+		// "application/vnd.taverna.scufl2.workflow+xml");
+		// odfPackage
+		// .setMediaType("application/vnd.taverna.scufl2.research-object");
+		// odfPackage.save("template.scufl2");
 
-		for (String odfFileEntry : new ArrayList<String>(
-				odfPackage.getFileEntries())) {
-			if (odfFileEntry.equals("/")) {
-				continue;
-			}
-			odfPackage.remove(odfFileEntry);
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		if (mimeType == null || !mimeType.contains("/")) {
+			throw new IllegalArgumentException("Invalid media type "
+					+ mimeType);
 		}
+		if (!ASCII.newEncoder().canEncode(mimeType)) {
+			throw new IllegalArgumentException("Media type must be ASCII: "
+					+ mimeType);
+		}
+		try {
+			mimeType.getBytes("ASCII");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.mimeType = mimeType;
+	}
 
-		odfPackage.insert(URI.create("foo.xml"), "workflows/deadbeef.xml",
-				"application/vnd.taverna.scufl2.workflow+xml");
-		odfPackage
-				.setMediaType("application/vnd.taverna.scufl2.research-object");
-		odfPackage.save("template.scufl2");
+	public void save(File bundleFile) throws IOException {
+		File tempFile = File.createTempFile(bundleFile.getName(), ".tmp",
+				bundleFile.getParentFile());
+		FileOutputStream fileOut = new FileOutputStream(tempFile);
+		ZipOutputStream zipOut = new ZipOutputStream(new BufferedOutputStream(
+				fileOut));
+		addMimeType(zipOut);
+		addManifest();
+		addFiles();
+		zipOut.flush();
+		zipOut.close();
+		tempFile.renameTo(bundleFile);
+	}
+
+	protected void addFiles() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void addManifest() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void addMimeType(ZipOutputStream zipOut) throws IOException {
+		ZipEntry entry = new ZipEntry(MIMETYPE);
+		entry.setMethod(ZipEntry.STORED);
+		byte[] mimeType = getMimeType().getBytes("ASCII");
+		entry.setSize(mimeType.length);
+		CRC32 crc = new CRC32();
+		crc.update(mimeType);
+		entry.setCrc(crc.getValue());
+		zipOut.putNextEntry(entry);
+		zipOut.write(mimeType);
+
 
 	}
 }
---
+++ scufl2-bundle/src/test/java/uk/org/taverna/scufl2/bundle/TestScufl2Bundle.java
@@ -0,0 +1,103 @@
+package uk.org.taverna.scufl2.bundle;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+public class TestScufl2Bundle {
+
+
+	private static final int MIME_OFFSET = 30;
+
+	@Test(expected = IllegalArgumentException.class)
+	public void mimeTypeInvalidCharset() throws Exception {
+		Scufl2Bundle scufl2Bundle = new Scufl2Bundle();
+		scufl2Bundle.setMimeType("food/brød");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void mimeTypeEmpty() throws Exception {
+		Scufl2Bundle scufl2Bundle = new Scufl2Bundle();
+		scufl2Bundle.setMimeType("");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void mimeTypeNoSlash() throws Exception {
+		Scufl2Bundle scufl2Bundle = new Scufl2Bundle();
+		scufl2Bundle.setMimeType("applicationtext");
+	}
+
+	@Test
+	public void defaultMimeType() throws Exception {
+		Scufl2Bundle scufl2Bundle = new Scufl2Bundle();
+		assertEquals(Scufl2Bundle.MIME_SCUFL2_BUNDLE,
+				scufl2Bundle.getMimeType());
+		File tmpFile = File.createTempFile("test", ".bundle");
+		assertTrue(tmpFile.delete());
+		System.out.println(tmpFile);
+		scufl2Bundle.save(tmpFile);
+		assertTrue(tmpFile.exists());
+		ZipFile zipFile = new ZipFile(tmpFile);
+		// Must be first entry
+		ZipEntry mimeEntry = zipFile.entries().nextElement();
+		assertEquals("First zip entry is not 'mimetype'", "mimetype",
+				mimeEntry.getName());
+		assertEquals("mimetype should be uncompressed, but compressed size mismatch", mimeEntry.getCompressedSize(), mimeEntry.getSize());
+		assertEquals("mimetype should have STORED method", ZipEntry.STORED, mimeEntry.getMethod());
+		assertEquals("Wrong mimetype", Scufl2Bundle.MIME_SCUFL2_BUNDLE,
+				IOUtils.toString(zipFile.getInputStream(mimeEntry)));
+
+		// Check position 30++ according to
+		// http://livedocs.adobe.com/navigator/9/Navigator_SDK9_HTMLHelp/wwhelp/wwhimpl/common/html/wwhelp.htm?context=Navigator_SDK9_HTMLHelp&file=Appx_Packaging.6.1.html#1522568
+		byte[] expected = ("mimetype" + Scufl2Bundle.MIME_SCUFL2_BUNDLE + "PK")
+				.getBytes("ASCII");
+		FileInputStream in = new FileInputStream(tmpFile);
+		assertEquals(MIME_OFFSET, in.skip(MIME_OFFSET));
+		byte[] actual = new byte[expected.length];
+		assertEquals(expected.length, in.read(actual));
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	public void workflowBundleMimeType() throws Exception {
+		Scufl2Bundle scufl2Bundle = new Scufl2Bundle();
+		scufl2Bundle.setMimeType(Scufl2Bundle.MIME_WORKFLOW_BUNDLE);
+		assertEquals(Scufl2Bundle.MIME_WORKFLOW_BUNDLE,
+				scufl2Bundle.getMimeType());
+		File tmpFile = File.createTempFile("test", ".bundle");
+		assertTrue(tmpFile.delete());
+		System.out.println(tmpFile);
+		scufl2Bundle.save(tmpFile);
+		ZipFile zipFile = new ZipFile(tmpFile);
+		ZipEntry mimeEntry = zipFile.getEntry("mimetype");
+		assertEquals("mimetype", mimeEntry.getName());
+		assertEquals("Wrong mimetype", Scufl2Bundle.MIME_WORKFLOW_BUNDLE,
+				IOUtils.toString(zipFile.getInputStream(mimeEntry)));
+
+	}
+
+	@Test
+	public void manifestMimetype() throws Exception {
+		Scufl2Bundle scufl2Bundle = new Scufl2Bundle();
+		scufl2Bundle.setMimeType(Scufl2Bundle.MIME_WORKFLOW_BUNDLE);
+
+		File tmpFile = File.createTempFile("test", ".bundle");
+		assertTrue(tmpFile.delete());
+		System.out.println(tmpFile);
+		scufl2Bundle.save(tmpFile);
+		ZipFile zipFile = new ZipFile(tmpFile);
+		ZipEntry manifestEntry = zipFile.getEntry("META-INF/manifest.xml");
+		InputStream manifestStream = zipFile.getInputStream(manifestEntry);
+
+	}
+
+}
