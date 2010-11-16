f6d9564e00278322c77ba30b73dabf6adacbd6fa
stain <stain@ralph.fb065306-1ecd-4974-a503-9c7871092168.net>
2010-11-16 13:12:00 +0000

    SCUFL2-30 activity ports with name and depths

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BeanshellActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BeanshellActivityParser.java
@@ -5,9 +5,12 @@ import java.net.URI;
 import uk.org.taverna.scufl2.api.activity.Activity;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.configurations.DataProperty;
+import uk.org.taverna.scufl2.api.port.InputActivityPort;
+import uk.org.taverna.scufl2.api.port.OutputActivityPort;
 import uk.org.taverna.scufl2.translator.t2flow.ParseException;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.translator.t2flow.T2Parser;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.ActivityInputPorts;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ActivityPortDefinitionBean;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.BeanshellConfig;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
@@ -55,7 +58,7 @@ public class BeanshellActivityParser extends AbstractActivityParser {
 				configBean, "xstream", BeanshellConfig.class);
 		Configuration configuration = new Configuration();
 		configuration.setConfigurationType(ACTIVITY_URI.resolve("#ConfigType"));
-		String script = beanshellConfig.getScript();		
+		String script = beanshellConfig.getScript();
 		DataProperty property = new DataProperty(
 				ACTIVITY_URI.resolve("#script"), script);
 
@@ -63,14 +66,31 @@ public class BeanshellActivityParser extends AbstractActivityParser {
 		Activity activity = getParserState().getCurrentActivity();
 		activity.getInputPorts().clear();
 		activity.getOutputPorts().clear();
-		for (ActivityPortDefinitionBean b : beanshellConfig.getInputs().getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityInputPortDefinitionBean()) {
-			System.out.println(b.getClass() + " " + b);
-			
+		for (ActivityPortDefinitionBean b : beanshellConfig
+				.getInputs()
+				.getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityInputPortDefinitionBean()) {
+			InputActivityPort a = new InputActivityPort();
+			a.setName(b.getName());
+			if (b.getDepth() != null) {
+				a.setDepth(b.getDepth().intValue());
+			}
+			a.setParent(activity);
 		}
-		for (ActivityPortDefinitionBean b : beanshellConfig.getOutputs().getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityOutputPortDefinitionBean()) {
-			System.out.println(b.getClass() + " " + b);
+		// TODO: Mime types, etc
+		for (ActivityPortDefinitionBean b : beanshellConfig
+				.getOutputs()
+				.getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityOutputPortDefinitionBean()) {
+			OutputActivityPort a = new OutputActivityPort();
+			a.setName(b.getName());
+			if (b.getDepth() != null) {
+				a.setDepth(b.getDepth().intValue());
+			}
+			if (b.getGranularDepth() != null) {
+				a.setGranularDepth(b.getGranularDepth().intValue());
+			}
+			a.setParent(activity);		
 		}
-		
+
 		configuration.getProperties().add(property);
 		return configuration;
 	}
