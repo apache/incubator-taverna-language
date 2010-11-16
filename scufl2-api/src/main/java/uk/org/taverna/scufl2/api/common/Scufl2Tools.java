5b796fab726b57eb07551e4eccd7c6546d12668c
stain <stain@ralph.fb065306-1ecd-4974-a503-9c7871092168.net>
2010-11-16 12:07:39 +0000

    SCUFL2-30 Scufl2Tools - can find processor bindings

---
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/common/Scufl2Tools.java
@@ -0,0 +1,116 @@
+package uk.org.taverna.scufl2.api.common;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import uk.org.taverna.scufl2.api.activity.Activity;
+import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.configurations.DataProperty;
+import uk.org.taverna.scufl2.api.configurations.Property;
+import uk.org.taverna.scufl2.api.core.Processor;
+import uk.org.taverna.scufl2.api.profiles.ProcessorBinding;
+import uk.org.taverna.scufl2.api.profiles.Profile;
+
+/**
+ * Utillity methods for dealing with SCUFL2 models
+ * 
+ * @author Stian Soiland-Reyes
+ *
+ */
+public class Scufl2Tools {
+
+	/**
+	 * Compare {@link ProcessorBinding}s by their {@link ProcessorBinding#getActivityPosition()}.
+	 * 
+	 * Note: this comparator imposes orderings that are inconsistent with equals.
+	 * 
+	 * @author Stian Soiland-Reyes
+	 *
+	 */
+	public static class BindingComparator implements
+			Comparator<ProcessorBinding> {
+
+		@Override
+		public int compare(ProcessorBinding o1, ProcessorBinding o2) {
+			return o1.getActivityPosition() - o2.getActivityPosition();
+		}
+
+	}
+
+	public String getPropertyData(List<Property> properties, URI predicate) {
+		for (Property prop : properties) {
+			if (prop.getPredicate().equals(predicate)) {
+				if (! (prop instanceof DataProperty)) {					
+					throw new IllegalStateException("Not a DataProperty: " + predicate);					
+				}
+				return ((DataProperty)prop).getDataValue();
+			}
+		}
+		throw new IndexOutOfBoundsException("Could not find property for "+ predicate);
+	}
+
+	public List<Configuration> configurationsFor(Configurable configurable, Profile profile) {
+		List<Configuration> configurations = new ArrayList<Configuration>();
+		for (Configuration config : profile.getConfigurations()) {
+			if (config.getConfigures().equals(configurable)) {
+				configurations.add(config);
+			}
+		}
+		return configurations;
+	}
+
+	public Configuration configurationFor(Configurable configurable, Profile profile) {
+		List<Configuration> configurations = configurationsFor(configurable, profile);
+		if (configurations.isEmpty()) { 
+			throw new IndexOutOfBoundsException("Could not find configuration for " + configurable);
+		}
+		if (configurations.size() > 1) {
+			throw new IllegalStateException("More than one configuration for " + configurable);
+		}
+		return configurations.get(0);
+	}
+
+
+	public List<ProcessorBinding> processorBindingsToActivity(Activity activity,
+			Profile profile) {
+		List<ProcessorBinding> bindings = new ArrayList<ProcessorBinding>();
+		for (ProcessorBinding pb : profile.getProcessorBindings()) {
+			if (pb.getBoundActivity().equals(activity)) {
+				bindings.add(pb);
+			}
+		}
+		Collections.sort(bindings, new BindingComparator());
+		return bindings;
+	}
+	
+
+	public List<ProcessorBinding> processorBindingsForProcessor(Processor processor,
+			Profile profile) {
+		List<ProcessorBinding> bindings = new ArrayList<ProcessorBinding>();
+		for (ProcessorBinding pb : profile.getProcessorBindings()) {
+			if (pb.getBoundProcessor().equals(processor)) {
+				bindings.add(pb);
+			}
+		}
+		Collections.sort(bindings, new BindingComparator());
+		return bindings;
+	}
+	
+	public ProcessorBinding processorBindingForProcessor(Processor processor,
+			Profile profile) {
+		List<ProcessorBinding> bindings = processorBindingsForProcessor(processor, profile);
+		if (bindings.isEmpty()) { 
+			throw new IndexOutOfBoundsException("Could not find bindings for " + processor);
+		}
+		if (bindings.size() > 1) {
+			throw new IllegalStateException("More than one proc binding for " + processor);
+		}
+		return bindings.get(0);
+	}
+
+	
+	
+}
--- scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/TestActivityParsing.java
+++ scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/TestActivityParsing.java
@@ -16,6 +16,8 @@ import uk.org.taverna.scufl2.api.activity.Activity;
 import uk.org.taverna.scufl2.api.common.Scufl2Tools;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Processor;
+import uk.org.taverna.scufl2.api.profiles.ProcessorBinding;
 import uk.org.taverna.scufl2.api.profiles.Profile;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.translator.t2flow.defaultactivities.BeanshellActivityParser;
@@ -29,16 +31,18 @@ public class TestActivityParsing {
 	@Test
 	public void readSimpleWorkflow() throws Exception {
 		URL wfResource = getClass().getResource(WF_ALL_ACTIVITIES);
-		assertNotNull("Could not find workflow " + WF_ALL_ACTIVITIES, wfResource);
+		assertNotNull("Could not find workflow " + WF_ALL_ACTIVITIES,
+				wfResource);
 		T2FlowParser parser = new T2FlowParser();
 		parser.setValidating(true);
-//		 parser.setStrict(true);
-		WorkflowBundle researchObj = parser.parseT2Flow(wfResource.openStream());
-//		System.out.println(researchObj.getProfiles().iterator().next()
-//				.getConfigurations());
+		// parser.setStrict(true);
+		WorkflowBundle researchObj = parser
+				.parseT2Flow(wfResource.openStream());
+		// System.out.println(researchObj.getProfiles().iterator().next()
+		// .getConfigurations());
 
 	}
-	
+
 	@Test
 	public void parseBeanshellScript() throws Exception {
 		URL wfResource = getClass().getResource(WF_AS);
@@ -46,18 +50,30 @@ public class TestActivityParsing {
 		T2FlowParser parser = new T2FlowParser();
 		parser.setValidating(true);
 		// parser.setStrict(true);
-		WorkflowBundle researchObj = parser.parseT2Flow(wfResource.openStream());
-//		System.out.println(researchObj.getProfiles().iterator().next()
-//				.getConfigurations());
+		WorkflowBundle researchObj = parser
+				.parseT2Flow(wfResource.openStream());
+		// System.out.println(researchObj.getProfiles().iterator().next()
+		// .getConfigurations());
 		Profile profile = researchObj.getProfiles().getByName("taverna-2.1.0");
-		for (Configuration config : profile.getConfigurations()) {
-			Activity a = (Activity) config.getConfigures();
-			if (a.getType().equals(BeanshellActivityParser.ACTIVITY_URI)) {
-				assertEquals(ACTIVITY_URI.resolve("#ConfigType"), config.getConfigurationType());
-				String script = scufl2Tools.getPropertyData(config.getProperties(), ACTIVITY_URI.resolve("#script"));
-			}
-		}
+		// Processors: [Workflow19, Echo_List, Concatenate_two_strings,
+		// Concatenate_two_strings_2, Concatenate_two_strings_3,
+		// Concatenate_two_strings_4, Create_Lots_Of_Strings, String_constant]
+		Processor concat = researchObj.getMainWorkflow().getProcessors()
+				.getByName("Concatenate_two_strings");
+		
+		ProcessorBinding binding = scufl2Tools.processorBindingForProcessor(concat, profile);
+		Activity bindingAct = binding.getBoundActivity();
+		Configuration config = scufl2Tools.configurationFor(binding.getBoundActivity(), profile);
+		
+		System.out.println(bindingAct.getName());
+		assertEquals(BeanshellActivityParser.ACTIVITY_URI, bindingAct.getType());
+		assertEquals(ACTIVITY_URI.resolve("#ConfigType"),
+				config.getConfigurationType());
+		String script = scufl2Tools.getPropertyData(config.getProperties(),
+				ACTIVITY_URI.resolve("#script"));
+		System.out.println(script);
+		System.out.println(bindingAct.getInputPorts());
+		System.out.println(bindingAct.getOutputPorts());
 	}
-
 
 }
