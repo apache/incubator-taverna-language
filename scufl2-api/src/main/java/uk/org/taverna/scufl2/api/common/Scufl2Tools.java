0bc3d596a12aa92b4c4106121124178ebe5e8018
stian@mygrid.org.uk <stian@mygrid.org.uk>
2010-12-08 14:32:32 +0000

    Multiple object/data properties
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12125 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/common/Scufl2Tools.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/common/Scufl2Tools.java
@@ -4,7 +4,9 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import uk.org.taverna.scufl2.api.activity.Activity;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
@@ -33,7 +35,7 @@ public class Scufl2Tools {
 	 *
 	 */
 	public static class BindingComparator implements
-			Comparator<ProcessorBinding> {
+	Comparator<ProcessorBinding> {
 
 		@Override
 		public int compare(ProcessorBinding o1, ProcessorBinding o2) {
@@ -42,31 +44,25 @@ public class Scufl2Tools {
 
 	}
 
-	public String getPropertyData(List<Property> properties, URI predicate) throws PropertyNotFoundExecption {
-		for (Property prop : properties) {
-			if (prop.getPredicate().equals(predicate)) {
-				if (! (prop instanceof DataProperty)) {					
-					throw new IllegalStateException("Not a DataProperty: " + predicate);					
-				}
-				return ((DataProperty)prop).getDataValue();
-			}
+	public Configuration configurationFor(Configurable configurable, Profile profile) {
+		List<Configuration> configurations = configurationsFor(configurable, profile);
+		if (configurations.isEmpty()) {
+			throw new IndexOutOfBoundsException("Could not find configuration for " + configurable);
 		}
-		throw new PropertyNotFoundExecption("Could not find property for "+ predicate);
+		if (configurations.size() > 1) {
+			throw new IllegalStateException("More than one configuration for " + configurable);
+		}
+		return configurations.get(0);
 	}
 
-	public ObjectProperty getPropertyObject(List<Property> properties, URI predicate) throws PropertyNotFoundExecption {
-		for (Property prop : properties) {
-			if (prop.getPredicate().equals(predicate)) {
-				if (! (prop instanceof ObjectProperty)) {					
-					throw new IllegalStateException("Not a ObjectProperty: " + predicate);					
-				}
-				return (ObjectProperty) prop;
-			}
-		}
-		throw new PropertyNotFoundExecption("Could not find property for "+ predicate);
+	public Configuration configurationForActivityBoundToProcessor(Processor concat,
+			Profile profile) {
+		ProcessorBinding binding = processorBindingForProcessor(concat, profile);
+		Configuration config = configurationFor(binding.getBoundActivity(), profile);
+		return config;
 	}
 
-	
+
 	public List<Configuration> configurationsFor(Configurable configurable, Profile profile) {
 		List<Configuration> configurations = new ArrayList<Configuration>();
 		for (Configuration config : profile.getConfigurations()) {
@@ -77,30 +73,72 @@ public class Scufl2Tools {
 		return configurations;
 	}
 
-	public Configuration configurationFor(Configurable configurable, Profile profile) {
-		List<Configuration> configurations = configurationsFor(configurable, profile);
-		if (configurations.isEmpty()) { 
-			throw new IndexOutOfBoundsException("Could not find configuration for " + configurable);
-		}
-		if (configurations.size() > 1) {
-			throw new IllegalStateException("More than one configuration for " + configurable);
-		}
-		return configurations.get(0);
-	}
-
-
-	public List<ProcessorBinding> processorBindingsToActivity(Activity activity,
-			Profile profile) {
-		List<ProcessorBinding> bindings = new ArrayList<ProcessorBinding>();
-		for (ProcessorBinding pb : profile.getProcessorBindings()) {
-			if (pb.getBoundActivity().equals(activity)) {
-				bindings.add(pb);
+	public String getPropertyData(List<Property> properties, URI predicate) throws PropertyNotFoundExecption {
+		for (Property prop : properties) {
+			if (prop.getPredicate().equals(predicate)) {
+				if (! (prop instanceof DataProperty)) {
+					throw new IllegalStateException("Not a DataProperty: " + predicate);
+				}
+				return ((DataProperty)prop).getDataValue();
 			}
 		}
-		Collections.sort(bindings, new BindingComparator());
-		return bindings;
+		throw new PropertyNotFoundExecption("Could not find property for "+ predicate);
 	}
-	
+
+	public Set<String> getPropertyDatas(List<Property> properties, URI predicate) {
+		Set<String> results = new HashSet<String>();
+		for (Property prop : properties) {
+			if (prop.getPredicate().equals(predicate)) {
+				if (!(prop instanceof ObjectProperty)) {
+					throw new IllegalStateException("Not a ObjectProperty: "
+							+ predicate);
+				}
+				results.add(((DataProperty) prop).getDataValue());
+			}
+		}
+		return results;
+	}
+
+
+	public ObjectProperty getPropertyObject(List<Property> properties, URI predicate) throws PropertyNotFoundExecption {
+		for (Property prop : properties) {
+			if (prop.getPredicate().equals(predicate)) {
+				if (! (prop instanceof ObjectProperty)) {
+					throw new IllegalStateException("Not a ObjectProperty: " + predicate);
+				}
+				return (ObjectProperty) prop;
+			}
+		}
+		throw new PropertyNotFoundExecption("Could not find property for "+ predicate);
+	}
+
+
+	public Set<ObjectProperty> getPropertyObjects(List<Property> properties,
+			URI predicate) {
+		Set<ObjectProperty> results = new HashSet<ObjectProperty>();
+		for (Property prop : properties) {
+			if (prop.getPredicate().equals(predicate)) {
+				if (!(prop instanceof ObjectProperty)) {
+					throw new IllegalStateException("Not a ObjectProperty: "
+							+ predicate);
+				}
+				results.add((ObjectProperty) prop);
+			}
+		}
+		return results;
+	}
+
+	public ProcessorBinding processorBindingForProcessor(Processor processor,
+			Profile profile) {
+		List<ProcessorBinding> bindings = processorBindingsForProcessor(processor, profile);
+		if (bindings.isEmpty()) {
+			throw new IndexOutOfBoundsException("Could not find bindings for " + processor);
+		}
+		if (bindings.size() > 1) {
+			throw new IllegalStateException("More than one proc binding for " + processor);
+		}
+		return bindings.get(0);
+	}
 
 	public List<ProcessorBinding> processorBindingsForProcessor(Processor processor,
 			Profile profile) {
@@ -113,26 +151,18 @@ public class Scufl2Tools {
 		Collections.sort(bindings, new BindingComparator());
 		return bindings;
 	}
-	
-	public ProcessorBinding processorBindingForProcessor(Processor processor,
-			Profile profile) {
-		List<ProcessorBinding> bindings = processorBindingsForProcessor(processor, profile);
-		if (bindings.isEmpty()) { 
-			throw new IndexOutOfBoundsException("Could not find bindings for " + processor);
+
+	public List<ProcessorBinding> processorBindingsToActivity(
+			Activity activity, Profile profile) {
+		List<ProcessorBinding> bindings = new ArrayList<ProcessorBinding>();
+		for (ProcessorBinding pb : profile.getProcessorBindings()) {
+			if (pb.getBoundActivity().equals(activity)) {
+				bindings.add(pb);
+			}
 		}
-		if (bindings.size() > 1) {
-			throw new IllegalStateException("More than one proc binding for " + processor);
-		}
-		return bindings.get(0);
+		Collections.sort(bindings, new BindingComparator());
+		return bindings;
 	}
 
-	public Configuration configurationForActivityBoundToProcessor(Processor concat,
-			Profile profile) {
-		ProcessorBinding binding = processorBindingForProcessor(concat, profile);		
-		Configuration config = configurationFor(binding.getBoundActivity(), profile);
-		return config;		
-	}
 
-	
-	
 }
