3c5913c01b5b0017cbf6329349c885b3dbd118a1
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-12-08 14:32:46 +0000

    Test new portdefinitions
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12126 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/TestActivityParsing.java
+++ scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/TestActivityParsing.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 
 import uk.org.taverna.scufl2.api.activity.Activity;
 import uk.org.taverna.scufl2.api.common.Scufl2Tools;
+import uk.org.taverna.scufl2.api.common.URITools;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.configurations.ObjectProperty;
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
@@ -66,8 +67,8 @@ public class TestActivityParsing {
 		// Concatenate_two_strings_4, Create_Lots_Of_Strings, String_constant]
 		Processor concat = researchObj.getMainWorkflow().getProcessors()
 				.getByName("Concatenate_two_strings");
-		
-		
+
+
 		Configuration concatConfig = scufl2Tools.configurationForActivityBoundToProcessor(concat, profile);
 		Activity concatAct = (Activity) concatConfig.getConfigures();
 		assertEquals(BeanshellActivityParser.ACTIVITY_URI, concatAct.getType());
@@ -76,49 +77,87 @@ public class TestActivityParsing {
 		String script = scufl2Tools.getPropertyData(concatConfig.getProperties(),
 				ACTIVITY_URI.resolve("#script"));
 		assertEquals("output = string1 + string2;", script);
-		
+
 		Set<String> expectedInputs = new HashSet<String>(Arrays.asList("string1", "string2"));
 		assertEquals(expectedInputs, concatAct.getInputPorts().getNames());
 		InputActivityPort s1 = concatAct.getInputPorts().getByName("string1");
 		assertEquals(0, s1.getDepth().intValue());
 		InputActivityPort s2 = concatAct.getInputPorts().getByName("string2");
-		assertEquals(0, s2.getDepth().intValue());		
-		
-		Configuration s1Config = scufl2Tools.configurationFor(s1, profile);
-		ObjectProperty dataType = scufl2Tools.getPropertyObject(s1Config.getProperties(), ACTIVITY_URI.resolve("#dataType"));		
-		assertEquals("java", dataType.getObjectUri().getScheme());
-		assertEquals("java.lang.String", dataType.getObjectUri().getSchemeSpecificPart());
+		assertEquals(0, s2.getDepth().intValue());
+
+		Set<ObjectProperty> inputDef = scufl2Tools.getPropertyObjects(
+				concatConfig.getProperties(),
+				ACTIVITY_URI.resolve("#inputPortDefinition"));
+		assertEquals(2, inputDef.size());
+
+		Set<URI> inputPorts = new HashSet<URI>();
+		for (InputActivityPort inPort : concatAct.getInputPorts()) {
+			inputPorts.add(new URITools().relativeUriForBean(inPort,
+					concatConfig));
+		}
+		// assertEquals(2, inputPorts.size());
+
+		for (ObjectProperty portDef : inputDef) {
+			assertEquals(ACTIVITY_URI.resolve("#InputPortDefinition"),
+					portDef.getObjectClass());
+			assertNull(portDef.getObjectUri());
+
+			ObjectProperty dataType = scufl2Tools.getPropertyObject(
+					portDef.getObjectProperties(),
+					ACTIVITY_URI.resolve("#dataType"));
+			assertEquals("java", dataType.getObjectUri().getScheme());
+			assertEquals("java.lang.String", dataType.getObjectUri()
+					.getSchemeSpecificPart());
+
+			ObjectProperty port = scufl2Tools.getPropertyObject(
+					portDef.getObjectProperties(),
+					ACTIVITY_URI.resolve("#definesInputPort"));
+			System.out.println(port.getObjectUri());
+
+		}
+
+
+
 		// TODO: Is java class here OK? It's a beanshell script after all..
-		
-		Set<String> expectedOutputs = new HashSet<String>(Arrays.asList("output"));		
+
+		Set<String> expectedOutputs = new HashSet<String>(Arrays.asList("output"));
 		assertEquals(expectedOutputs, concatAct.getOutputPorts().getNames());
 		OutputActivityPort out = concatAct.getOutputPorts().getByName("output");
 		assertEquals(0, out.getDepth().intValue());
 
-		Configuration outConfig = scufl2Tools.configurationFor(out, profile);
+		Set<ObjectProperty> outputDef = scufl2Tools.getPropertyObjects(
+				concatConfig.getProperties(),
+				ACTIVITY_URI.resolve("#outputPortDefinition"));
+		assertEquals(1, outputDef.size());
+		ObjectProperty out1Def = outputDef.iterator().next();
+		assertEquals(ACTIVITY_URI.resolve("#OutputPortDefinition"),
+				out1Def.getObjectClass());
+
 		// FIXME: mimetype should be an annotation?
 		// FIXME: This should not be in the Beanshell namespace
-		ObjectProperty mimeType = scufl2Tools.getPropertyObject(outConfig.getProperties(), ACTIVITY_URI.resolve("#expectedMimeType"));
+		ObjectProperty mimeType = scufl2Tools.getPropertyObject(
+				out1Def.getObjectProperties(),
+				ACTIVITY_URI.resolve("#expectedMimeType"));
 		assertEquals(URI.create("http://purl.org/NET/mediatypes/text/plain"), mimeType.getObjectUri());
-		
-		
+
+
 		Processor echoList = researchObj.getMainWorkflow().getProcessors()
 		.getByName("Echo_List");
 		Configuration echoConfig = scufl2Tools.configurationForActivityBoundToProcessor(echoList, profile);
 		Activity echoAct = (Activity) echoConfig.getConfigures();
-		
+
 		expectedInputs = new HashSet<String>(Arrays.asList("inputlist"));
 		assertEquals(expectedInputs, echoAct.getInputPorts().getNames());
 		InputActivityPort inputList = echoAct.getInputPorts().getByName("inputlist");
 		assertEquals(1, inputList.getDepth().intValue());
-		
-		expectedOutputs = new HashSet<String>(Arrays.asList("outputlist"));		
+
+		expectedOutputs = new HashSet<String>(Arrays.asList("outputlist"));
 		assertEquals(expectedOutputs, echoAct.getOutputPorts().getNames());
 		OutputActivityPort outputList = echoAct.getOutputPorts().getByName("outputlist");
 		assertEquals(1, outputList.getDepth().intValue());
-		
-		
-		
+
+
+
 	}
 
 }
