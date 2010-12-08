4205bd5f7545ba9502363af4461bbbe14250f8d4
stian@mygrid.org.uk <stian@mygrid.org.uk>
2010-12-08 14:30:21 +0000

    URI-tools
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12116 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/common/URITools.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/common/URITools.java
@@ -1,5 +1,17 @@
 package uk.org.taverna.scufl2.api.common;
 
+import java.net.URI;
+
 public class URITools {
+
+	public URI relativeUriForBean(WorkflowBean bean, WorkflowBean relativeToBean) {
+		URI rootUri = uriForBean(relativeToBean);
+		URI beanUri = uriForBean(bean);
+		return rootUri.relativize(beanUri);
+	}
+
+	private URI uriForBean(WorkflowBean bean) {
+
+	}
 
 }
