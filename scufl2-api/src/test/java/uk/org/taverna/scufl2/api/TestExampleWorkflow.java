b3e6cf4ce207ab69517d0b43247962de45bc1b15
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-01-05 16:36:02 +0000

    Make sure ExampleWorkflow can be created

---
+++ scufl2-api/src/test/java/uk/org/taverna/scufl2/api/TestExampleWorkflow.java
@@ -0,0 +1,12 @@
+package uk.org.taverna.scufl2.api;
+
+import org.junit.Test;
+
+public class TestExampleWorkflow extends ExampleWorkflow {
+
+	@Test
+	public void makeflowBundle() throws Exception {
+		makeWorkflowBundle();
+	}
+
+}
