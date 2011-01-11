c6a69ad5f8a37a8be4f8ebd793bb46cf8c66f0f4
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-01-11 12:58:27 +0000

    RDF media type

--- scufl2-rdfxml/src/main/java/uk/org/taverna/scufl2/rdfxml/RdfXMLReader.java
+++ scufl2-rdfxml/src/main/java/uk/org/taverna/scufl2/rdfxml/RdfXMLReader.java
@@ -7,12 +7,18 @@ import java.text.ParseException;
 import java.util.Collections;
 import java.util.Set;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
 import uk.org.taverna.scufl2.api.io.WorkflowBundleReader;
 
 public class RdfXMLReader implements WorkflowBundleReader {
 
 	public static final String APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE = "application/vnd.taverna.scufl2.workflow-bundle";
+
+	public static final String APPLICATION_RDF_XML = "application/rdf+xml";
+
 
 	@Override
 	public Set<String> getMediaTypes() {
@@ -23,6 +29,7 @@ public class RdfXMLReader implements WorkflowBundleReader {
 	@Override
 	public WorkflowBundle readBundle(File bundleFile, String mediaType)
 			throws ParseException, IOException {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
