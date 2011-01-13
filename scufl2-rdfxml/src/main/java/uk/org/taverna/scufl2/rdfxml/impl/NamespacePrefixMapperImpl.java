f3e04fe2b23e650a6ab2164980d85b620c954326
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-01-13 14:54:22 +0000

    More XML prefixes

--- scufl2-rdfxml/src/main/java/uk/org/taverna/scufl2/rdfxml/impl/NamespacePrefixMapperImpl.java
+++ scufl2-rdfxml/src/main/java/uk/org/taverna/scufl2/rdfxml/impl/NamespacePrefixMapperImpl.java
@@ -4,20 +4,35 @@ import com.sun.xml.internal.bind.marshaller.NamespacePrefixMapper;
 
 @SuppressWarnings("restriction")
 public class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
-	
+
 	public String getPreferredPrefix(String namespaceUri, String suggestion,
 			boolean requirePrefix) {
-		if ("http://www.w3.org/2001/XMLSchema-instance".equals(namespaceUri)) {
+		if (namespaceUri.equals("http://www.w3.org/2001/XMLSchema-instance")) {
 			return "xsi";
 		}
-		if ("http://ns.taverna.org.uk/2010/scufl2#".equals(namespaceUri)) {
-			return "scufl2";
+		if (namespaceUri.equals("http://ns.taverna.org.uk/2010/scufl2#")) {
+			return ""; // default
+		}
+		if (namespaceUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#")) {
+			return "rdf";
+		}
+		if (namespaceUri.equals("http://www.w3.org/2000/01/rdf-schema#")) {
+			return "rdfs";
+		}
+		if (namespaceUri.equals("http://purl.org/dc/elements/1.1/")) {
+			return "dc";
+		}
+		if (namespaceUri.equals("http://purl.org/dc/terms/")) {
+			return "dcterms";
+		}
+		if (namespaceUri.equals("http://www.w3.org/2002/07/owl#")) {
+			return "owl";
 		}
 		return suggestion;
 	}
 
 	public String[] getPreDeclaredNamespaceUris() {
-		return new String[] { "urn:xsi", "urn:scufl2" };
+		return new String[] {  };
 	}
 
 }
