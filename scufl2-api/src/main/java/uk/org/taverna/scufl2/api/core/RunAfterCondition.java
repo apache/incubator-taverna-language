1b3a605162af6040c7a9263a2a9b7c36b1b1310b
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-01-21 10:51:56 +0000

    RunAfterCondition constructor
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12246 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/core/RunAfterCondition.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/core/RunAfterCondition.java
@@ -2,8 +2,14 @@ package uk.org.taverna.scufl2.api.core;
 
 /**
  * @author Alan R Williams
- *
+ * @author Stian Soiland-Reyes
+ * 
  */
 public class RunAfterCondition extends ProcessorControlledStartCondition {
+
+	public RunAfterCondition(Processor parent, Processor runAfter) {
+		setParent(parent);
+		setControllingProcessor(runAfter);
+	}
 
 }
