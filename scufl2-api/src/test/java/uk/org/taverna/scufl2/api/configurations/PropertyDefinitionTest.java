702d961f3d3f10b1cbff1c0f93daac452abce51e
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-02-01 16:20:32 +0000

    Added PropertyReferenceDefinition split out getOptions() to now be Set<String> on Literal and Set<URI> on PropRefDef

--- scufl2-api/src/test/java/uk/org/taverna/scufl2/api/configurations/PropertyDefinitionTest.java
+++ scufl2-api/src/test/java/uk/org/taverna/scufl2/api/configurations/PropertyDefinitionTest.java
@@ -1,26 +1,25 @@
 /*******************************************************************************
- * Copyright (C) 2010 The University of Manchester   
- * 
+ * Copyright (C) 2010 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  ******************************************************************************/
 package uk.org.taverna.scufl2.api.configurations;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -33,11 +32,11 @@ import org.junit.Test;
 
 /**
  * Unit tests for {@link PropertyDefinition}.
- * 
+ *
  * @author David Withers
  */
 public class PropertyDefinitionTest {
-	
+
 	private PropertyDefinition propertyDefinition;
 
 	private URI activityURI;
@@ -45,58 +44,14 @@ public class PropertyDefinitionTest {
 	@Before
 	public void setUp() throws Exception {
 		activityURI = URI.create("http://ns.taverna.org.uk/2010/activity/test");
-		propertyDefinition = new PropertyDefinition(activityURI.resolve("#testProperty"), "name", "label", "description", true, false, false, new String[0]) {
+		propertyDefinition = new PropertyDefinition(
+				activityURI.resolve("#testProperty"), "name", "label",
+				"description", true, false, false) {
+			@Override
 			protected String toString(String indent) {
 				return "";
-			}			
+			}
 		};
-	}
-
-	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#PropertyDefinition(java.net.URI, java.net.URI, java.lang.String, java.lang.String, boolean, boolean, java.lang.String[])}.
-	 */
-	@Test
-	public void testPropertyDefinition() {
-		PropertyDefinition propertyDefinition = new PropertyDefinition(activityURI.resolve("#testProperty"), "name", "A", "B", true, true, true, new String[] {"yes", "no"}) {
-			protected String toString(String indent) {
-				return "";
-			}			
-		};
-		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
-		assertEquals("name", propertyDefinition.getName());
-		assertEquals("A", propertyDefinition.getLabel());
-		assertEquals("B", propertyDefinition.getDescription());
-		assertTrue(propertyDefinition.isRequired());
-		assertTrue(propertyDefinition.isMultiple());
-		assertTrue(propertyDefinition.isOrdered());
-		assertArrayEquals(new String[] {"yes", "no"}, propertyDefinition.getOptions());
-	}
-
-	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getPredicate()}.
-	 */
-	@Test
-	public void testGetPredicate() {
-		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
-		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
-	}
-
-	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getName()}.
-	 */
-	@Test
-	public void testGetName() {
-		assertEquals("name", propertyDefinition.getName());
-		assertEquals("name", propertyDefinition.getName());
-	}
-
-	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getLabel()}.
-	 */
-	@Test
-	public void testGetLabel() {
-		assertEquals("label", propertyDefinition.getLabel());
-		assertEquals("label", propertyDefinition.getLabel());
 	}
 
 	/**
@@ -109,12 +64,30 @@ public class PropertyDefinitionTest {
 	}
 
 	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#isRequired()}.
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getLabel()}.
 	 */
 	@Test
-	public void testIsRequired() {
-		assertTrue(propertyDefinition.isRequired());
-		assertTrue(propertyDefinition.isRequired());
+	public void testGetLabel() {
+		assertEquals("label", propertyDefinition.getLabel());
+		assertEquals("label", propertyDefinition.getLabel());
+	}
+
+	/**
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		assertEquals("name", propertyDefinition.getName());
+		assertEquals("name", propertyDefinition.getName());
+	}
+
+	/**
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getPredicate()}.
+	 */
+	@Test
+	public void testGetPredicate() {
+		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
+		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
 	}
 
 	/**
@@ -136,12 +109,34 @@ public class PropertyDefinitionTest {
 	}
 
 	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#getOptions()}.
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#isRequired()}.
 	 */
 	@Test
-	public void testGetOptions() {
-		assertArrayEquals(new String[0], propertyDefinition.getOptions());
-		assertArrayEquals(new String[0], propertyDefinition.getOptions());
+	public void testIsRequired() {
+		assertTrue(propertyDefinition.isRequired());
+		assertTrue(propertyDefinition.isRequired());
+	}
+
+	/**
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#PropertyDefinition(java.net.URI, java.net.URI, java.lang.String, java.lang.String, boolean, boolean, java.lang.String[])}.
+	 */
+	@Test
+	public void testPropertyDefinition() {
+		PropertyDefinition propertyDefinition = new PropertyDefinition(
+				activityURI.resolve("#testProperty"), "name", "label",
+				"description", true, true, true) {
+			@Override
+			protected String toString(String indent) {
+				return "";
+			}
+		};
+		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
+		assertEquals("name", propertyDefinition.getName());
+		assertEquals("label", propertyDefinition.getLabel());
+		assertEquals("description", propertyDefinition.getDescription());
+		assertTrue(propertyDefinition.isRequired());
+		assertTrue(propertyDefinition.isMultiple());
+		assertTrue(propertyDefinition.isOrdered());
 	}
 
 	/**
@@ -151,5 +146,5 @@ public class PropertyDefinitionTest {
 	public void testToString() {
 		assertNotNull(propertyDefinition.toString());
 	}
-	
+
 }
--- scufl2-api/src/test/java/uk/org/taverna/scufl2/api/configurations/PropertyLiteralDefinitionTest.java
+++ scufl2-api/src/test/java/uk/org/taverna/scufl2/api/configurations/PropertyLiteralDefinitionTest.java
@@ -1,31 +1,32 @@
 /*******************************************************************************
- * Copyright (C) 2010 The University of Manchester   
- * 
+ * Copyright (C) 2010 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  ******************************************************************************/
 package uk.org.taverna.scufl2.api.configurations;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,19 +35,53 @@ import uk.org.taverna.scufl2.api.property.PropertyLiteral;
 
 /**
  * Unit tests for {@link PropertyLiteralDefinition}.
- * 
+ *
  * @author David Withers
  */
 public class PropertyLiteralDefinitionTest {
 
 	private PropertyLiteralDefinition propertyLiteralDefinition;
-	
+
 	private URI activityURI;
 
 	@Before
 	public void setUp() throws Exception {
 		activityURI = URI.create("http://ns.taverna.org.uk/2010/activity/test");
 		propertyLiteralDefinition = new PropertyLiteralDefinition(activityURI.resolve("#testProperty"), PropertyLiteral.XSD_STRING, "n", "l", "d", false, false, false);
+	}
+
+	/**
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyLiteralDefinition#getLiteralType()}.
+	 */
+	@Test
+	public void testGetLiteralType() {
+		assertEquals(PropertyLiteral.XSD_STRING, propertyLiteralDefinition.getLiteralType());
+		assertEquals(PropertyLiteral.XSD_STRING, propertyLiteralDefinition.getLiteralType());
+	}
+
+	/**
+	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyDefinition#PropertyDefinition(java.net.URI, java.net.URI, java.lang.String, java.lang.String, boolean, boolean, java.lang.String[])}.
+	 */
+	@Test
+	public void testPropertyDefinition() {
+		LinkedHashSet<String> options = new LinkedHashSet<String>();
+		options.addAll(Arrays.asList("Yes", "No"));
+		PropertyLiteralDefinition propertyDefinition = new PropertyLiteralDefinition(
+				activityURI.resolve("#testProperty"), null, "name", "label",
+				"description", true, true, true, options) {
+			@Override
+			protected String toString(String indent) {
+				return "";
+			}
+		};
+		assertEquals(activityURI.resolve("#testProperty"), propertyDefinition.getPredicate());
+		assertEquals("name", propertyDefinition.getName());
+		assertEquals("label", propertyDefinition.getLabel());
+		assertEquals("description", propertyDefinition.getDescription());
+		assertTrue(propertyDefinition.isRequired());
+		assertTrue(propertyDefinition.isMultiple());
+		assertTrue(propertyDefinition.isOrdered());
+		assertEquals(options, propertyDefinition.getOptions());
 	}
 
 	/**
@@ -63,7 +98,7 @@ public class PropertyLiteralDefinitionTest {
 		assertFalse(propertyLiteralDefinition.isRequired());
 		assertFalse(propertyLiteralDefinition.isMultiple());
 		assertFalse(propertyLiteralDefinition.isOrdered());
-		assertArrayEquals(new String[0], propertyLiteralDefinition.getOptions());
+		assertTrue(propertyLiteralDefinition.getOptions().isEmpty());
 	}
 
 	/**
@@ -71,7 +106,10 @@ public class PropertyLiteralDefinitionTest {
 	 */
 	@Test
 	public void testPropertyLiteralDefinitionURIURIStringStringBooleanBooleanStringArray() {
-		PropertyLiteralDefinition propertyLiteralDefinition = new PropertyLiteralDefinition(activityURI.resolve("#testProperty"), PropertyLiteral.XSD_INTEGER, "name", "x", "y", false, true, false, new String[] {"A", "Z"});
+		LinkedHashSet<String> options = new LinkedHashSet<String>();
+		options.addAll(Arrays.asList("Yes", "No"));
+
+		PropertyLiteralDefinition propertyLiteralDefinition = new PropertyLiteralDefinition(activityURI.resolve("#testProperty"), PropertyLiteral.XSD_INTEGER, "name", "x", "y", false, true, false, options);
 		assertEquals(activityURI.resolve("#testProperty"), propertyLiteralDefinition.getPredicate());
 		assertEquals(PropertyLiteral.XSD_INTEGER, propertyLiteralDefinition.getLiteralType());
 		assertEquals("name", propertyLiteralDefinition.getName());
@@ -80,16 +118,7 @@ public class PropertyLiteralDefinitionTest {
 		assertFalse(propertyLiteralDefinition.isRequired());
 		assertTrue(propertyLiteralDefinition.isMultiple());
 		assertFalse(propertyLiteralDefinition.isOrdered());
-		assertArrayEquals(new String[] {"A", "Z"}, propertyLiteralDefinition.getOptions());
-	}
-
-	/**
-	 * Test method for {@link uk.org.taverna.scufl2.api.configurations.PropertyLiteralDefinition#getLiteralType()}.
-	 */
-	@Test
-	public void testGetLiteralType() {
-		assertEquals(PropertyLiteral.XSD_STRING, propertyLiteralDefinition.getLiteralType());
-		assertEquals(PropertyLiteral.XSD_STRING, propertyLiteralDefinition.getLiteralType());
+		assertEquals(options, propertyLiteralDefinition.getOptions());
 	}
 
 	/**
