06c22e866d7fd1ec1d828fa37dc884523290600a
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-07 09:41:14 +0000

    Added constructor that doesn't require options to be specified
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12469 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/configurations/PropertyReferenceDefinition.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/configurations/PropertyReferenceDefinition.java
@@ -44,6 +44,34 @@ public class PropertyReferenceDefinition extends PropertyDefinition {
 	 * @param ordered
 	 *            whether the order of multiple instances of the
 	 *            <code>Property</code> is significant
+	 */
+	public PropertyReferenceDefinition(URI predicate, String name,
+			String label, String description, boolean required,
+			boolean multiple, boolean ordered) {
+		super(predicate, name, label, description, required, multiple, ordered);
+		options = new LinkedHashSet<URI>();
+	}
+
+	/**
+	 * Create a definition of an {@link PropertyReference}.
+	 * 
+	 * @param predicate
+	 *            the URI identifying the <code>PropertyReference</code> that
+	 *            this class defines
+	 * @param name
+	 *            the name of the <code>PropertyReference</code>
+	 * @param label
+	 *            a human readable label for the <code>PropertyReference</code>
+	 * @param description
+	 *            a description of the <code>PropertyReference</code>
+	 * @param required
+	 *            whether the <code>PropertyReference</code> is mandatory
+	 * @param multiple
+	 *            whether there can be multiple instances of the
+	 *            <code>PropertyReference</code>
+	 * @param ordered
+	 *            whether the order of multiple instances of the
+	 *            <code>Property</code> is significant
 	 * @param options
 	 *            Set of options
 	 */
