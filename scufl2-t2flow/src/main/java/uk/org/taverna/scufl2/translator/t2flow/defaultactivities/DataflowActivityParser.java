b6f89631406d27c39f7d446287aa22f4c789cf3d
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-10 13:44:49 +0000

    Skeleton parsing of workflows first (to facilitate nested workflows). Give better names to workflows/activities/processor bindings/configurations (need updated NameSet)
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12531 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/DataflowActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/DataflowActivityParser.java
@@ -2,7 +2,10 @@ package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
 import java.net.URI;
 
+import uk.org.taverna.scufl2.api.common.URITools;
+import uk.org.taverna.scufl2.api.common.WorkflowBean;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.core.Workflow;
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
@@ -10,12 +13,14 @@ import uk.org.taverna.scufl2.xml.t2flow.jaxb.DataflowConfig;
 
 public class DataflowActivityParser extends AbstractActivityParser {
 
+	private URITools uriTools = new URITools();
+	
 	private static URI activityRavenURI = T2FlowParser.ravenURI
 			.resolve("net.sf.taverna.t2.activities/dataflow-activity/");
 
 	private static String activityClassName = "net.sf.taverna.t2.activities.dataflow.DataflowActivity";
 
-	public static URI scufl2Uri = URI
+	public static URI nestedUri = URI
 			.create("http://ns.taverna.org.uk/2010/activity/nested-workflow");
 
 	@Override
@@ -27,7 +32,7 @@ public class DataflowActivityParser extends AbstractActivityParser {
 
 	@Override
 	public URI mapT2flowRavenIdToScufl2URI(URI t2flowActivity) {
-		return scufl2Uri;
+		return nestedUri;
 	}
 
 	@Override
@@ -35,19 +40,24 @@ public class DataflowActivityParser extends AbstractActivityParser {
 			ConfigBean configBean) throws ReaderException {
 		DataflowConfig dataflowConfig = unmarshallConfig(t2FlowParser,
 				configBean, "dataflow", DataflowConfig.class);
-		String dataflowReference = dataflowConfig.getRef();
-
 		Configuration configuration = new Configuration();
+		configuration.setConfigurableType(nestedUri.resolve("#Config"));		
 
-		// ConfigurablePropertyConfiguration configurablePropertyConfiguration =
-		// new ConfigurablePropertyConfiguration();
-		// configurablePropertyConfiguration.setParent(configuration);
-		// ConfigurableProperty configuredProperty = new ConfigurableProperty(
-		// scufl2Uri.resolve("#workflow").toASCIIString());
-		// configurablePropertyConfiguration
-		// .setConfiguredProperty(configuredProperty);
-		// configurablePropertyConfiguration.setValue(dataflowReference);
+		String wfId = dataflowConfig.getRef();
+		URI wfUri = Workflow.WORKFLOW_ROOT.resolve(wfId + "/");
+		Workflow wf = (Workflow) getUriTools().resolveUri(wfUri, getParserState().getCurrentWorkflowBundle());		
+		URI uri = getUriTools().relativeUriForBean(wf, getParserState().getCurrentWorkflowBundle());
+
+		configuration.getPropertyResource().addPropertyReference(nestedUri.resolve("#workflow"), uri);		
 		return configuration;
+	}
+
+	public void setUriTools(URITools uriTools) {
+		this.uriTools = uriTools;
+	}
+
+	public URITools getUriTools() {
+		return uriTools;
 	}
 
 }
