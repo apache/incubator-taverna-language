9ca6967cbee6a086415a3f7421e9bc65bb0fc2b6
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-10 11:09:07 +0000

    parse later
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12511 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/ParserState.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/ParserState.java
@@ -1,10 +1,17 @@
 package uk.org.taverna.scufl2.translator.t2flow;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import uk.org.taverna.scufl2.api.common.WorkflowBean;
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
 import uk.org.taverna.scufl2.api.core.Processor;
 import uk.org.taverna.scufl2.api.core.Workflow;
+import uk.org.taverna.scufl2.api.impl.LazyMap;
 import uk.org.taverna.scufl2.api.profiles.ProcessorBinding;
 import uk.org.taverna.scufl2.api.profiles.Profile;
+import uk.org.taverna.scufl2.translator.t2flow.ParserState.ParseLaterCallback;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.Activity;
 
 public class ParserState {
 	private uk.org.taverna.scufl2.api.activity.Activity currentActivity;
@@ -73,5 +80,37 @@ public class ParserState {
 	private WorkflowBundle currentResearchObject;
 	private T2Parser currentT2Parser;
 	private Workflow currentWorkflow;
+	private T2FlowParser t2FlowParser;
+	private Map<Class<?>, Map<Object, ParseLaterCallback>> parseLater = new LazyMap<Class<?>, Map<Object,ParseLaterCallback>>() {
+		@Override
+		public Map<Object, ParseLaterCallback> getDefault(Class<?> key) {
+			return new HashMap<Object, ParserState.ParseLaterCallback>();
+		}
+	};
 
+	public final T2FlowParser getT2FlowParser() {
+		return t2FlowParser;
+	}
+
+	public void setT2FlowParser(T2FlowParser t2FlowParser) {
+		this.t2FlowParser = t2FlowParser;
+		
+	}
+
+	public Map<Object, ParseLaterCallback> getParseLater(Class<?> type) {
+		return parseLater.get(type);
+	}
+
+	public interface ParseLaterCallback<Type> {
+		public void parsed(Type original, WorkflowBean parsed, ParserState parserState);
+	}
+
+
+	public void parseLater(Object elementToParse,
+			ParseLaterCallback callback) {
+		parseLater.get(elementToParse.getClass()).put(elementToParse, callback);
+		
+	}
+
+	
 }
