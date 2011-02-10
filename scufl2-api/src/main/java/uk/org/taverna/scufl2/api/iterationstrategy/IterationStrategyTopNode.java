d8927e5637cf936a5c97eb5e8dd0a02d87a023bc
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-02-10 17:08:39 +0000

    ITerationStrategyTopNode is also a List<IterationStrategyNode> and an IterationStrategyParent

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/iterationstrategy/IterationStrategyTopNode.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/iterationstrategy/IterationStrategyTopNode.java
@@ -1,10 +1,12 @@
 package uk.org.taverna.scufl2.api.iterationstrategy;
 
+import java.util.List;
+
 /**
  * @author Stian Soiland-Reyes
- * 
+ *
  */
-public interface IterationStrategyTopNode extends
- IterationStrategyNode {
+public interface IterationStrategyTopNode extends IterationStrategyNode,
+		List<IterationStrategyNode>, IterationStrategyParent {
 
 }
