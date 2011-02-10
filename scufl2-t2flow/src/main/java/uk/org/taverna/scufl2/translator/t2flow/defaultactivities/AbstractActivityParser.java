2bfb6eeaa01e0f25d3a545f50552f0483f3f8b41
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-10 11:09:41 +0000

    dispatch stack parsers
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12512 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/AbstractActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/AbstractActivityParser.java
@@ -1,10 +1,15 @@
 package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
+import java.io.StringReader;
+
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
 
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.translator.t2flow.ParserState;
@@ -32,14 +37,41 @@ public abstract class AbstractActivityParser implements T2Parser {
 				|| !configBean.getEncoding().equals(encoding)) {
 			throw new ReaderException("Unsupported config bean " + configBean);
 		}
+		return unmarshallElement(t2FlowParser, (Element)config, configType);
+	}
+	
+	public <ConfigType> ConfigType unmarshallElement(T2FlowParser t2FlowParser,
+			Element element, Class<ConfigType> configType)
+			throws ReaderException {
+		
 		Unmarshaller unmarshaller2 = t2FlowParser.getUnmarshaller();
 		unmarshaller2.setSchema(null);
 		JAXBElement<ConfigType> configElemElem;
 		try {
-			configElemElem = unmarshaller2.unmarshal((Element) config,
+			configElemElem = unmarshaller2.unmarshal(element,
 					configType);
 		} catch (JAXBException e) {
-			throw new ReaderException("Can't parse config bean " + configBean, e);
+			throw new ReaderException("Can't parse element " + element, e);
+		}
+
+		return configElemElem.getValue();
+	}
+	
+	public <ConfigType> ConfigType unmarshallXml(T2FlowParser t2FlowParser,
+			String xml, Class<ConfigType> configType)
+			throws ReaderException {
+		
+		Unmarshaller unmarshaller2 = t2FlowParser.getUnmarshaller();
+		unmarshaller2.setSchema(null);
+		
+		JAXBElement<ConfigType> configElemElem;
+		
+		Source source = new StreamSource(new StringReader(xml));		
+		try {
+			configElemElem = unmarshaller2.unmarshal(source,
+					configType);
+		} catch (JAXBException e) {
+			throw new ReaderException("Can't parse xml " + xml, e);
 		}
 
 		return configElemElem.getValue();
---
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultdispatchstack/InvokeParser.java
@@ -0,0 +1,38 @@
+package uk.org.taverna.scufl2.translator.t2flow.defaultdispatchstack;
+
+import java.net.URI;
+
+import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
+import uk.org.taverna.scufl2.translator.t2flow.defaultactivities.AbstractActivityParser;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
+
+public class InvokeParser extends AbstractActivityParser {
+
+	private static URI ravenURI =
+		T2FlowParser.ravenURI.resolve("net.sf.taverna.t2.core/workflowmodel-impl/");
+
+	private static String className = "net.sf.taverna.t2.workflowmodel.processor.dispatch.layers.Invoke";
+
+	public static URI scufl2Uri = URI
+			.create("http://ns.taverna.org.uk/2010/scufl2/taverna/dispatchlayer/Invoke");
+
+	@Override
+	public boolean canHandlePlugin(URI pluginURI) {
+		String uriStr = pluginURI.toASCIIString();
+		return uriStr.startsWith(ravenURI.toASCIIString())
+				&& uriStr.endsWith(className);
+	}
+
+	@Override
+	public URI mapT2flowRavenIdToScufl2URI(URI t2flowActivity) {
+		return scufl2Uri;
+	}
+
+	@Override
+	public Configuration parseConfiguration(T2FlowParser t2FlowParser,
+			ConfigBean configBean) throws ReaderException {
+		return null;
+	}
+}
