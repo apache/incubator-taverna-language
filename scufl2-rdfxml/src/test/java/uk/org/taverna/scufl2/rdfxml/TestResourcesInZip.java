ca7de9616a6133500dbc0dea5ca267c1b50cb8a0
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-11 16:26:03 +0000

    Store using the same UCFPackage.
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12545 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-rdfxml/src/test/java/uk/org/taverna/scufl2/rdfxml/TestResourcesInZip.java
+++ scufl2-rdfxml/src/test/java/uk/org/taverna/scufl2/rdfxml/TestResourcesInZip.java
@@ -1,6 +1,7 @@
 package uk.org.taverna.scufl2.rdfxml;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static uk.org.taverna.scufl2.rdfxml.TestRDFXMLWriter.APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE;
 
@@ -33,16 +34,39 @@ public class TestResourcesInZip {
 	@Test
 	public void singleFile() throws Exception {
 		UCFPackage resources = originalBundle.getResources();
+		assertNull(resources.getPackageMediaType());
 		resources.addResource("Hello there", "hello.txt", "text/plain");
 		File bundleFile = tempFile();
-		bundleIO.writeBundle(originalBundle, bundleFile, APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE);
+		bundleIO.writeBundle(originalBundle, bundleFile, APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE);		
+		assertEquals(APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE, resources.getPackageMediaType());
+		assertEquals(1, resources.getRootFiles().size());
+		assertEquals("workflowBundle.rdf", resources.getRootFiles().get(0).getPath());
 		
 		ZipFile zipFile = new ZipFile(bundleFile);
 		ZipEntry hello = zipFile.getEntry("hello.txt");
 		assertEquals("hello.txt", hello.getName());
 		assertEquals("Hello there",
-				IOUtils.toString(zipFile.getInputStream(hello), "ASCII"));
+				IOUtils.toString(zipFile.getInputStream(hello), "ASCII"));		
+	}
+	
+	@Test
+	public void differentMediaType() throws Exception {
+		UCFPackage resources = originalBundle.getResources();
+		resources.setPackageMediaType("application/x-something-else");
+		assertEquals("application/x-something-else", resources.getPackageMediaType());
 		
+		resources.addResource("Hello there", "hello.txt", "text/plain");
+		File bundleFile = tempFile();
+		bundleIO.writeBundle(originalBundle, bundleFile, APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE);
+		assertEquals("application/x-something-else", resources.getPackageMediaType());
+		assertEquals(0, resources.getRootFiles().size());
+		// RDFXMLWriter does not touch the rootFile or media type if it's non-null
+		
+		ZipFile zipFile = new ZipFile(bundleFile);
+		ZipEntry hello = zipFile.getEntry("hello.txt");
+		assertEquals("hello.txt", hello.getName());
+		assertEquals("Hello there",
+				IOUtils.toString(zipFile.getInputStream(hello), "ASCII"));		
 	}
 	
 	public File tempFile() throws IOException {
