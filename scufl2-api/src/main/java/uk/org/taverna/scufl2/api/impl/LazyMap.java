744d2e18585d3e0092bbda197bc39706c3dd726c
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-02-15 15:10:04 +0000

    LazyMap now based on TreeMap.

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/impl/LazyMap.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/impl/LazyMap.java
@@ -1,10 +1,12 @@
 package uk.org.taverna.scufl2.api.impl;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
- * A lazy map, inspired by org.apache.commons.collections.map.LazyMap
+ * A lazy TreeMap, inspired by org.apache.commons.collections.map.LazyMap
  * <p>
  * On {@link #get(Object)}, if a key is not found in the map,
  * {@link #getDefault(Object)} will be called to create the value for the given
@@ -14,17 +16,37 @@ import java.util.Map;
  * <p>
  * Implementations of this class must implement {@link #getDefault(Object)} to
  * specify the default value to create.
- * 
+ *
  * @author Stian Soiland-Reyes
- * 
+ *
  * @param <KeyType>
  *            Type of key
  * @param <ValueType>
  *            Type of value
  */
 public abstract class LazyMap<KeyType, ValueType> extends
-HashMap<KeyType, ValueType> implements Map<KeyType, ValueType> {
+		TreeMap<KeyType, ValueType> implements Map<KeyType, ValueType> {
 
+	private static final long serialVersionUID = 3284689384208221667L;
+
+	public LazyMap() {
+		super();
+	}
+
+	public LazyMap(Comparator<? super KeyType> comparator) {
+		super(comparator);
+	}
+
+	public LazyMap(Map<? extends KeyType, ? extends ValueType> m) {
+		super(m);
+	}
+
+	public LazyMap(SortedMap<KeyType, ? extends ValueType> m) {
+		super(m);
+	}
+
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public ValueType get(Object key) {
 		ValueType value = super.get(key);
