610fb74a0e09472dc169d856de082dc5669cb89b
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-17 14:12:30 +0000

    default configuration type is #Config - not #ConfigType
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12613 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/configurations/ConfigurationDefinition.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/configurations/ConfigurationDefinition.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2010 The University of Manchester   
- * 
+ * Copyright (C) 2010 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -27,7 +27,7 @@ import uk.org.taverna.scufl2.api.property.PropertyResource;
 /**
  * Definition of the {@link Configuration} required to configure a
  * {@link uk.org.taverna.scufl2.api.common.Configurable Configurable}.
- * 
+ *
  * @author David Withers
  */
 public class ConfigurationDefinition {
@@ -47,14 +47,15 @@ public class ConfigurationDefinition {
 	 * Creates the definition of the {@link Configuration} required to configure the
 	 * {@link uk.org.taverna.scufl2.api.common.Configurable Configurable} specified by the
 	 * configurableType.
-	 * 
+	 *
 	 * @param configurableType
 	 *            the URI that identifies the <code>Configurable</code> type
 	 */
 	public ConfigurationDefinition(URI configurableType) {
 		this.configurableType = configurableType;
 		propertyResourceDefinition = new PropertyResourceDefinition();
-		propertyResourceDefinition.setTypeURI(configurableType.resolve("#ConfigType"));
+		propertyResourceDefinition.setTypeURI(configurableType
+				.resolve("#Config"));
 		propertyResourceDefinition.setDescription("");
 		propertyResourceDefinition.setRequired(true);
 		propertyResourceDefinition.setMultiple(false);
@@ -64,7 +65,7 @@ public class ConfigurationDefinition {
 	/**
 	 * Returns the URI that identifies the {@link uk.org.taverna.scufl2.api.common.Configurable
 	 * Configurable} type.
-	 * 
+	 *
 	 * @return the URI that identifies the <code>Configurable</code> type
 	 */
 	public URI getConfigurableType() {
@@ -72,25 +73,25 @@ public class ConfigurationDefinition {
 	}
 
 	/**
-	 * Sets the URI that identifies the {@link uk.org.taverna.scufl2.api.common.Configurable
-	 * Configurable} type.
-	 * 
-	 * @param configurableType
-	 *            the URI that identifies the <code>Configurable</code> type
-	 */
-	public void setConfigurableType(URI configurableType) {
-		this.configurableType = configurableType;
-	}
-
-	/**
 	 * Returns the definition of the {@link PropertyResource} required by the
 	 * <code>Configuration</code>.
-	 * 
+	 *
 	 * @return the definition of the {@link PropertyResource} required by the
 	 *         <code>Configuration</code>
 	 */
 	public PropertyResourceDefinition getPropertyResourceDefinition() {
 		return propertyResourceDefinition;
+	}
+
+	/**
+	 * Sets the URI that identifies the {@link uk.org.taverna.scufl2.api.common.Configurable
+	 * Configurable} type.
+	 *
+	 * @param configurableType
+	 *            the URI that identifies the <code>Configurable</code> type
+	 */
+	public void setConfigurableType(URI configurableType) {
+		this.configurableType = configurableType;
 	}
 
 	@Override
