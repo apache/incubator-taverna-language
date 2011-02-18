94537af89378c7e91248d465a1bf3f8158c157cb
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-18 15:30:24 +0000

    Fixed URI for input/output splitters.
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12640 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/WSDLXMSplitterParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/WSDLXMSplitterParser.java
@@ -1,7 +1,8 @@
 package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
-import java.net.URI;
 import static uk.org.taverna.scufl2.translator.t2flow.defaultactivities.WSDLActivityParser.WSDL;
+
+import java.net.URI;
 
 import uk.org.taverna.scufl2.api.activity.Activity;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
@@ -11,7 +12,6 @@ import uk.org.taverna.scufl2.api.property.PropertyResource;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ActivityPortDefinitionBean;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
-import uk.org.taverna.scufl2.xml.t2flow.jaxb.WSDLConfig;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.XMLSplitterConfig;
 
 public class WSDLXMSplitterParser extends AbstractActivityParser {
@@ -22,9 +22,9 @@ public class WSDLXMSplitterParser extends AbstractActivityParser {
 	private static String inputSplitterClassName = "net.sf.taverna.t2.activities.wsdl.xmlsplitter.XMLInputSplitterActivity";
 	private static String outputSplitterClassName = "net.sf.taverna.t2.activities.wsdl.xmlsplitter.XMLOutputSplitterActivity";
 
-	public static URI SPLITTER = WSDL.resolve("xml-splitter");
-	public static URI XML_INPUT_SPLITTER = SPLITTER.resolve("xml-splitter/in");
-	public static URI XML_OUTPUT_SPLITTER = SPLITTER.resolve("xml-splitter/out");
+	public static URI SPLITTER = WSDL.resolve("wsdl/xml-splitter");
+	public static URI XML_INPUT_SPLITTER = WSDL.resolve("xml-splitter/in");
+	public static URI XML_OUTPUT_SPLITTER = WSDL.resolve("xml-splitter/out");
 
 	@Override
 	public boolean canHandlePlugin(URI activityURI) {
