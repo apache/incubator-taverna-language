fd198d7d37b94dd111104f91ea53b0db872fdccf
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-02-22 12:03:38 +0000

    test dispatch layer

--- scufl2-rdfxml/src/test/java/uk/org/taverna/scufl2/rdfxml/TestRDFXMLReader.java
+++ scufl2-rdfxml/src/test/java/uk/org/taverna/scufl2/rdfxml/TestRDFXMLReader.java
@@ -1,17 +1,21 @@
 package uk.org.taverna.scufl2.rdfxml;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static uk.org.taverna.scufl2.api.io.structure.StructureReader.TEXT_VND_TAVERNA_SCUFL2_STRUCTURE;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Processor;
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
 
@@ -30,6 +34,16 @@ public class TestRDFXMLReader {
 		assertNotNull("Can't find example workflow bundle " + name, exampleBundle);
 		workflowBundle = bundleIO.readBundle(exampleBundle, APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE);
 	}
+
+	@Test
+	public void dispatchStack() throws Exception {
+		Processor hello = workflowBundle.getMainWorkflow().getProcessors().getByName("Hello");
+		assertEquals("http://ns.taverna.org.uk/2010/taverna/2.2/DefaultDispatchStack", hello.getDispatchStack().getType().toASCIIString());
+		assertEquals(7, hello.getDispatchStack().size());
+		assertEquals("http://ns.taverna.org.uk/2010/scufl2/taverna#Failover", hello.getDispatchStack().get(3).getConfigurableType().toASCIIString());
+	}
+
+
 	
 	@Test
 	public void testParsedWorkflow() throws Exception {
@@ -37,9 +51,21 @@ public class TestRDFXMLReader {
 		
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
 		bundleIO.writeBundle(workflowBundle, output, TEXT_VND_TAVERNA_SCUFL2_STRUCTURE);
+		
+		
 		String bundleTxt = new String(output.toByteArray(), "UTF-8");
 		
 		assertEquals(testWorkflowBundleIO.getStructureFormatWorkflowBundle(), bundleTxt);
+		
+	}
+		
+	@Ignore
+	@Test
+	public void xmlOutput() throws Exception {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		new RDFXMLSerializer(workflowBundle).workflowDoc(output, workflowBundle.getMainWorkflow(), URI.create("workflows/HelloWorld.rdf"));		
+		String bundleTxt = new String(output.toByteArray(), "UTF-8");
+		System.out.println(bundleTxt);
 		
 	}
 	
