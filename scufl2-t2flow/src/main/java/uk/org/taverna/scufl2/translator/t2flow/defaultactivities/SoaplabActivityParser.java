e8705565b984fa32796634977f1386407ebcef8d
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-03-09 12:48:32 +0000

    Implemented Soaplab parser.
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@12752 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/SoaplabActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/SoaplabActivityParser.java
@@ -1,19 +1,23 @@
 package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
+import java.math.BigInteger;
 import java.net.URI;
 
 import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.api.property.PropertyLiteral;
+import uk.org.taverna.scufl2.api.property.PropertyResource;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.SoaplabConfig;
 
 public class SoaplabActivityParser extends AbstractActivityParser {
-	private static URI activityRavenURI =
-			T2FlowParser.ravenURI.resolve("net.sf.taverna.t2.activities/soaplab-activity/");
+	private static URI activityRavenURI = T2FlowParser.ravenURI
+			.resolve("net.sf.taverna.t2.activities/soaplab-activity/");
 
 	private static String activityClassName = "net.sf.taverna.t2.activities.soaplab.SoaplabActivity";
 
-	public static URI scufl2Uri = URI
-			.create("http://ns.taverna.org.uk/2010/activity/soaplab");
+	public static URI scufl2Uri = URI.create("http://ns.taverna.org.uk/2010/activity/soaplab");
 
 	@Override
 	public boolean canHandlePlugin(URI activityURI) {
@@ -28,10 +32,40 @@ public class SoaplabActivityParser extends AbstractActivityParser {
 	}
 
 	@Override
-	public Configuration parseConfiguration(T2FlowParser t2FlowParser,
-			ConfigBean configBean) {
-		// TODO Auto-generated method stub
-		return null;
+	public Configuration parseConfiguration(T2FlowParser t2FlowParser, ConfigBean configBean)
+			throws ReaderException {
+		SoaplabConfig soaplabConfig = unmarshallConfig(t2FlowParser, configBean, "xstream",
+				SoaplabConfig.class);
+
+		Configuration configuration = new Configuration();
+		configuration.setParent(getParserState().getCurrentProfile());
+
+		PropertyResource configResource = configuration.getPropertyResource();
+		configResource.setTypeURI(scufl2Uri.resolve("#Config"));
+
+		String endpoint = soaplabConfig.getEndpoint();
+		if (endpoint == null || endpoint.equals("")) {
+			throw new ReaderException("Soablab config has no endpoint set");
+		}
+		configResource.addPropertyAsString(scufl2Uri.resolve("#endpoint"), endpoint);
+
+		double pollingBackoff = soaplabConfig.getPollingBackoff();
+		configResource.addProperty(scufl2Uri.resolve("#pollingBackoff"), new PropertyLiteral(
+				pollingBackoff));
+
+		BigInteger pollingInterval = soaplabConfig.getPollingInterval();
+		if (pollingInterval != null) {
+			configResource.addProperty(scufl2Uri.resolve("#pollingInterval"), new PropertyLiteral(
+					pollingInterval.intValue()));
+		}
+
+		BigInteger pollingIntervalMax = soaplabConfig.getPollingIntervalMax();
+		if (pollingIntervalMax != null) {
+			configResource.addProperty(scufl2Uri.resolve("#pollingIntervalMax"),
+					new PropertyLiteral(pollingIntervalMax.intValue()));
+		}
+
+		return configuration;
 	}
 
 }
