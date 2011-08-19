fd5a092b070f6ac87972a462b568d4977e1e9e7e
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-19 16:48:03 +0000

    Project containing code to validate SCUFL 2 structurally so that a workflow is OK to run
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13762 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-validation-structural/src/test/java/uk/org/taverna/scufl2/validation/structural/WorkflowTest.java
@@ -0,0 +1,31 @@
+package uk.org.taverna.scufl2.validation.structural;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Workflow;
+import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.validation.ValidationException;
+
+
+public class WorkflowTest {
+	
+	@Test
+	public void testIncompleteWorkflow() throws ReaderException, IOException {
+    	ReportStructuralValidationEventListener l = new ReportStructuralValidationEventListener();
+		WorkflowBundle wb = new WorkflowBundle();
+		Workflow w = new Workflow();
+		wb.setMainWorkflow(w);
+	    StructuralValidator sv = new StructuralValidator();
+			sv.checkStructure(wb, l);
+			ValidatorState vs = sv.getValidatorState();
+			assertEquals(1, l.getIncompleteWorkflows().size());
+			assert(l.getIncompleteWorkflows().contains(w));	    
+	}
+	
+
+}
