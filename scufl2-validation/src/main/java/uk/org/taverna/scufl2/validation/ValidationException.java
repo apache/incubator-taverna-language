f6486c1ff454700b5b9047cc7a10ad6e1707daee
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-19 16:45:49 +0000

    Project containing code to validate SCUFL 2
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13761 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-validation/src/main/java/uk/org/taverna/scufl2/validation/ValidationException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package uk.org.taverna.scufl2.validation;
+
+/**
+ * @author alanrw
+ *
+ */
+public class ValidationException extends Exception {
+
+	public ValidationException(String string) {
+		super(string);
+	}
+
+}
