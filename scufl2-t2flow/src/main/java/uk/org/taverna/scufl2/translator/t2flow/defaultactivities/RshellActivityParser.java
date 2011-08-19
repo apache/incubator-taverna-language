a50225ea94644a62a307272e8c701b82e99f6f2a
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-08-19 17:16:46 +0100

    SCUFL2-51 - use regex to extract strange xpath 'symantic types'

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/RshellActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/RshellActivityParser.java
@@ -1,10 +1,22 @@
 package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static uk.org.taverna.scufl2.translator.t2flow.defaultactivities.AbstractActivityParser.PORT_DEFINITION;
 
 import uk.org.taverna.scufl2.api.activity.Activity;
+import uk.org.taverna.scufl2.api.common.URITools;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.api.port.InputActivityPort;
+import uk.org.taverna.scufl2.api.port.OutputActivityPort;
+import uk.org.taverna.scufl2.api.property.PropertyException;
 import uk.org.taverna.scufl2.api.property.PropertyLiteral;
 import uk.org.taverna.scufl2.api.property.PropertyResource;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
@@ -12,10 +24,21 @@ import uk.org.taverna.scufl2.xml.t2flow.jaxb.ActivityPortDefinitionBean;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.RShellConfig;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.RShellSymanticType;
-import uk.org.taverna.scufl2.xml.t2flow.jaxb.RetryConfig;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.RShellSymanticType.RShellPortSymanticTypeBean;
 
 public class RshellActivityParser extends AbstractActivityParser {
 
+	/*
+	* A lovely artifact of xstream 'efficiency' - Xpath backpointers to previous elements.
+	* luckily we are here restricted within this specific config bean
+	* 
+	* Example:
+	* ../../net.sf.taverna.t2.activities.rshell.RShellPortSymanticTypeBean[3]/symanticType
+	* ../../../inputSymanticTypes/net.sf.taverna.t2.activities.rshell.RShellPortSymanticTypeBean[2]/symanticType
+	 */
+	Pattern strangeXpath = Pattern.compile("[./]+/(inputSymanticTypes)?.*TypeBean(\\[(\\d+)\\])?/symanticType$");
+
+	
 	private static URI activityRavenURI =
 			T2FlowParser.ravenURI.resolve("net.sf.taverna.t2.activities/rshell-activity/");
 
@@ -83,22 +106,116 @@ public class RshellActivityParser extends AbstractActivityParser {
 		Activity activity = getParserState().getCurrentActivity();
 		activity.getInputPorts().clear();
 		activity.getOutputPorts().clear();
+		Map<URI, PropertyResource> portDefs = new HashMap<URI, PropertyResource>();
 		for (ActivityPortDefinitionBean portBean : rshellConfig
-				.getInputs().getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityInputPortDefinitionBean()
-				) {
-			parseAndAddInputPortDefinition(portBean, configuration, activity);
+				.getInputs()
+				.getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityInputPortDefinitionBean()) {
+			PropertyResource portDef = parseAndAddInputPortDefinition(portBean,
+					configuration, activity);
+			try {
+				URI portURI = portDef.getPropertyAsResourceURI(PORT_DEFINITION
+						.resolve("#definesInputPort"));
+				portDefs.put(portURI, portDef);
+			} catch (PropertyException ex) {
+				throw new ReaderException("Did not define port in " + portDef);
+			}
 		}
 		for (ActivityPortDefinitionBean portBean : rshellConfig
 				.getOutputs()
 				.getNetSfTavernaT2WorkflowmodelProcessorActivityConfigActivityOutputPortDefinitionBean()) {
-			parseAndAddOutputPortDefinition(portBean, configuration, activity);			
+			PropertyResource portDef = parseAndAddOutputPortDefinition(
+					portBean, configuration, activity);
+			try {
+
+				URI portURI = portDef.getPropertyAsResourceURI(PORT_DEFINITION
+						.resolve("#definesOutputPort"));
+				portDefs.put(portURI, portDef);
+			} catch (PropertyException ex) {
+				throw new ReaderException("Did not define port in " + portDef);
+			}
+
 		}
 		
-		// TODO: Semantic types
-		
 		RShellSymanticType inputSymanticTypes = rshellConfig.getInputSymanticTypes();
-		inputSymanticTypes.getNetSfTavernaT2ActivitiesRshellRShellPortSymanticTypeBean();
-		
+		List<String> foundInputTypes = new ArrayList<String>();
+		for (RShellPortSymanticTypeBean  symanticType: inputSymanticTypes.getNetSfTavernaT2ActivitiesRshellRShellPortSymanticTypeBean()) {
+			String portName = symanticType.getName();
+			InputActivityPort symanticPort = getParserState().getCurrentActivity().getInputPorts().getByName(portName);
+			URI portUri = new URITools().relativeUriForBean(symanticPort, configuration);
+			
+			PropertyResource portDef = portDefs.get(portUri);
+			if (portDef == null) {
+				throw new ReaderException("Can't find the port definition for " + portUri);
+			}
+			String symanticValue = symanticType.getSymanticType().getValue();
+			
+			String reference = symanticType.getSymanticType().getReference();
+			if (reference != null) {
+
+				Matcher matcher = strangeXpath.matcher(reference);
+				if (matcher == null || ! matcher.matches()) {
+					throw new ReaderException("Unhandled xstream xpath expression: " + reference);
+				}								
+				String position = matcher.group(3);
+				if (position == null) {
+					position = "1";
+				}
+				symanticValue = foundInputTypes.get(Integer.parseInt(position)-1);
+			}
+			
+			foundInputTypes.add(symanticValue); // Even if it's null - so the index is correct
+			if (symanticValue != null) {
+				portDef.addPropertyReference(PORT_DEFINITION.resolve("#dataType"),
+						ACTIVITY_URI.resolve("#" + symanticValue));
+			}
+		}
+		// FIXME: Avoid this repetition. Would require a fair bit of parser state..
+		RShellSymanticType outputSymanticTypes = rshellConfig.getOutputSymanticTypes();
+		List<String> foundOutputTypes = new ArrayList<String>();
+		for (RShellPortSymanticTypeBean  symanticType: outputSymanticTypes.getNetSfTavernaT2ActivitiesRshellRShellPortSymanticTypeBean()) {
+			String portName = symanticType.getName();
+			OutputActivityPort symanticPort = getParserState().getCurrentActivity().getOutputPorts().getByName(portName);
+			URI portUri = new URITools().relativeUriForBean(symanticPort, configuration);
+			
+			PropertyResource portDef = portDefs.get(portUri);
+			if (portDef == null) {
+				throw new ReaderException("Can't find the port definition for " + portUri);
+			}
+			String symanticValue = symanticType.getSymanticType().getValue();
+			
+			String reference = symanticType.getSymanticType().getReference();
+			if (reference != null) {
+				// A lovely artifact of xstream 'efficiency' - Xpath backpointers to previous elements.
+				// luckily we are here restricted within this specific config bean
+				// 
+				// Example:
+				// ../../net.sf.taverna.t2.activities.rshell.RShellPortSymanticTypeBean[3]/symanticType
+				// ../../../inputSymanticTypes/net.sf.taverna.t2.activities.rshell.RShellPortSymanticTypeBean[2]/symanticType
+				
+				Matcher matcher = strangeXpath.matcher(reference);
+				if (matcher == null || ! matcher.matches()) {
+					throw new ReaderException("Unhandled xstream xpath expression: " + reference);
+				}
+								
+				boolean isInputSymantic = matcher.group(1) != null;
+				String position = matcher.group(3);
+				if (position == null) {
+					position = "1";
+				}
+				if (isInputSymantic) {
+					symanticValue = foundInputTypes.get(Integer.parseInt(position)-1);
+				} else {
+					symanticValue = foundOutputTypes.get(Integer.parseInt(position)-1);
+				}
+			}
+			
+			foundOutputTypes.add(symanticValue); // Even if it's null - so the index is correct
+			if (symanticValue != null) {
+				portDef.addPropertyReference(PORT_DEFINITION.resolve("#dataType"),
+						ACTIVITY_URI.resolve("#" + symanticValue));
+			}
+		}
+
 		
 		return configuration;
 		
