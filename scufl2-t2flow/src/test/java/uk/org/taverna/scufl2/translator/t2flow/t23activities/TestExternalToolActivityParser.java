4ae066edb049509565e2b075cb7f878437133850
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-24 16:41:52 +0000

    Basic external tool schema and parsing
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13818 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/t23activities/TestExternalToolActivityParser.java
+++ scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/t23activities/TestExternalToolActivityParser.java
@@ -1,10 +1,19 @@
 package uk.org.taverna.scufl2.translator.t2flow.t23activities;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+
 import javax.xml.bind.JAXBException;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import uk.org.taverna.scufl2.api.common.Scufl2Tools;
+import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Processor;
+import uk.org.taverna.scufl2.api.profiles.Profile;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 
 public class TestExternalToolActivityParser {
@@ -21,6 +30,24 @@ public class TestExternalToolActivityParser {
 		parser = new T2FlowParser();
 		parser.setValidating(true);
 		parser.setStrict(true);
+		
+	}
+	
+	
+	@Test
+	public void parse2_2() throws Exception {
+		URL wfResource = getClass().getResource(WF_2_2);
+		assertNotNull("Could not find workflow " + WF_2_2, wfResource);
+		WorkflowBundle bundle = parser
+				.parseT2Flow(wfResource.openStream());
+		Profile profile = bundle.getMainProfile();
+		Processor proc = bundle.getMainWorkflow().getProcessors()
+				.getByName("cat");
+		assertNotNull(proc);
+		Configuration config = scufl2Tools
+				.configurationForActivityBoundToProcessor(proc, profile);
+		assertNotNull(config);
+		// TODO: Check data types defined (semantic types)
 		
 	}
 	
