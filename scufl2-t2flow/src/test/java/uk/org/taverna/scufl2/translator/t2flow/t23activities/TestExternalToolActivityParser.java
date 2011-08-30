913c8ed2eb45e084f49a8e375e499fd8a17339d0
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-08-30 14:48:09 +0000

    SCUFL2-85 Test tool-2-3.xsd
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13865 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/t23activities/TestExternalToolActivityParser.java
+++ scufl2-t2flow/src/test/java/uk/org/taverna/scufl2/translator/t2flow/t23activities/TestExternalToolActivityParser.java
@@ -243,6 +243,7 @@ public class TestExternalToolActivityParser {
 		assertNotNull("Could not find workflow " + WF_2_3, wfResource);
 		WorkflowBundle bundle = parser
 				.parseT2Flow(wfResource.openStream());
+		
 		Profile profile = bundle.getMainProfile();
 		Processor proc = bundle.getMainWorkflow().getProcessors()
 				.getByName("Tool");
@@ -253,8 +254,189 @@ public class TestExternalToolActivityParser {
 		assertEquals(ACTIVITY_URI.resolve("#Config"), 
 				config.getConfigurableType());
 		PropertyResource resource = config.getPropertyResource();
-		// Should NOT have a toolId
 		assertFalse(resource.getProperties().containsKey(ACTIVITY_URI.resolve("#toolId")));
+		assertFalse(resource.getProperties().containsKey(ACTIVITY_URI.resolve("#edited")));
+
+		assertEquals(ACTIVITY_URI.resolve("#local"),  
+				resource.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#mechanismType")));
+		assertEquals("default local",  
+				resource.getPropertyAsString(ACTIVITY_URI.resolve("#mechanismName")));
+	
+		
+		String mechanismXML = resource.getPropertyAsLiteral(ACTIVITY_URI.resolve("#mechanismXML")).getLiteralValue();
+		assertXpathEquals("", mechanismXML, "/localInvocation");
+		
+		PropertyResource description = resource.getPropertyAsResource(ACTIVITY_URI.resolve("#toolDescription"));
+		assertEquals("someName",  
+				description.getPropertyAsString(DC.resolve("title")));
+		assertEquals("some group",  
+				description.getPropertyAsString(ACTIVITY_URI.resolve("#category")));
+		assertEquals("the description\n  goes here",  
+				description.getPropertyAsString(DC.resolve("description")));
+		assertEquals("cat %%theString%% to %%stringReplace%% file ",  
+				description.getPropertyAsString(ACTIVITY_URI.resolve("#command")));
+		assertEquals(1200, description.getPropertyAsLiteral(ACTIVITY_URI.resolve("#preparingTimeoutInSeconds")).getLiteralValueAsInt());
+		assertEquals(1800, description.getPropertyAsLiteral(ACTIVITY_URI.resolve("#executionTimeoutInSeconds")).getLiteralValueAsInt());
+		
+		assertTrue(description.getProperties().get(ACTIVITY_URI.resolve("#tag")).isEmpty());
+		assertTrue(description.getProperties().get(ACTIVITY_URI.resolve("#runtimeEnvironment")).isEmpty());
+		assertTrue(description.getProperties().get(ACTIVITY_URI.resolve("#queue")).isEmpty());
+		
+		assertEquals(false, description.getPropertyAsLiteral(
+				ACTIVITY_URI.resolve("#includeStdIn")).getLiteralValueAsBoolean());
+		assertEquals(true, description.getPropertyAsLiteral(
+				ACTIVITY_URI.resolve("#includeStdOut")).getLiteralValueAsBoolean());
+		assertEquals(true, description.getPropertyAsLiteral(
+				ACTIVITY_URI.resolve("#includeStdErr")).getLiteralValueAsBoolean());
+		assertTrue(description.getProperties().get(ACTIVITY_URI.resolve("#validReturnCode")).isEmpty());
+		
+		// Check ports
+
+		Activity activity = (Activity) config.getConfigures();
+		System.out.println(activity.getInputPorts().getNames());
+		assertEquals(6, activity.getInputPorts().size());
+		
+		
+		InputActivityPort file1 = activity.getInputPorts().getByName("file1");
+		assertNotNull("Could not find activity input port first_file", file1);
+		assertEquals(Integer.valueOf(0), file1.getDepth());
+
+		InputActivityPort file2 = activity.getInputPorts().getByName("file2");
+		assertNotNull("Could not find activity input port first_file", file2);
+		assertEquals(Integer.valueOf(0), file2.getDepth());
+
+		InputActivityPort fileList = activity.getInputPorts().getByName("fileList");
+		assertNotNull("Could not find activity input port first_file", fileList);
+		assertEquals(Integer.valueOf(1), file2.getDepth());
+
+		InputActivityPort fileList2 = activity.getInputPorts().getByName("fileList2");
+		assertNotNull("Could not find activity input port first_file", fileList2);
+		assertEquals(Integer.valueOf(1), file2.getDepth());
+		
+		InputActivityPort stringReplace = activity.getInputPorts().getByName("stringReplace");
+		assertNotNull("Could not find activity input port first_file", stringReplace);
+		assertEquals(Integer.valueOf(0), stringReplace.getDepth());		
+		
+		InputActivityPort stringReplaceWithDifferentPort = activity.getInputPorts().getByName("stringReplaceWithDifferentPort");
+		assertNotNull("Could not find activity input port first_file", stringReplaceWithDifferentPort);
+		assertEquals(Integer.valueOf(0), stringReplace.getDepth());
+		
+		// No STDIN as includeStdIn is false
+		
+		assertEquals(4, activity.getOutputPorts().size());
+		OutputActivityPort stdout = activity.getOutputPorts().getByName("STDOUT");
+		assertNotNull("Could not find output port STDOUT", stdout);
+		assertEquals(Integer.valueOf(0), stdout.getDepth());
+		
+		OutputActivityPort stderr = activity.getOutputPorts().getByName("STDERR");
+		assertNotNull("Could not find output port STDERR", stderr);
+		assertEquals(Integer.valueOf(0), stderr.getDepth());
+		
+		OutputActivityPort out1 = activity.getOutputPorts().getByName("out1");
+		assertNotNull("Could not find output port out1", out1);
+		assertEquals(Integer.valueOf(0), out1.getDepth());
+		
+		OutputActivityPort out2 = activity.getOutputPorts().getByName("out2");
+		assertNotNull("Could not find output port out2", out2);
+		assertEquals(Integer.valueOf(0), out2.getDepth());
+		
+		// Check port definitions
+		
+		// 1 - file1
+		PropertyResource portDefinition = scufl2Tools.portDefinitionFor(file1, profile);
+		assertNotNull("Could not find port definition for file1", portDefinition);
+		assertEquals(ACTIVITY_URI.resolve("#binary"),
+				portDefinition.getPropertyAsResourceURI(PORT_DEFINITION.resolve("#dataType")));
+		assertEquals(ACTIVITY_URI.resolve("#File"), 
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#substitutionType")));
+		assertEquals("file1",
+				portDefinition.getPropertyAsString(ACTIVITY_URI.resolve("#substitutes")));		
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#charset")));
+
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#forceCopy")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#concatenate")));
+
+		// 2 - file2
+		portDefinition = scufl2Tools.portDefinitionFor(file2, profile);
+		assertNotNull("Could not find port definition for file2", portDefinition);
+		assertEquals(PropertyLiteral.XSD_STRING,
+				portDefinition.getPropertyAsResourceURI(PORT_DEFINITION.resolve("#dataType")));
+		assertEquals(ACTIVITY_URI.resolve("#File"), 
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#substitutionType")));
+		assertEquals("anotherFile.txt",
+				portDefinition.getPropertyAsString(ACTIVITY_URI.resolve("#substitutes")));	
+		assertTrue(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#charset")));
+		assertEquals(CHARSET.resolve("#windows-1252"),
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#charset")));
+
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#forceCopy")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#concatenate")));
+
+		
+		// 3 - fileList
+		portDefinition = scufl2Tools.portDefinitionFor(fileList, profile);
+		assertNotNull("Could not find port definition for fileList", portDefinition);
+		assertEquals(PropertyLiteral.XSD_STRING,
+				portDefinition.getPropertyAsResourceURI(PORT_DEFINITION.resolve("#dataType")));
+		assertEquals(ACTIVITY_URI.resolve("#File"), 
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#substitutionType")));
+		assertEquals("fileList",
+				portDefinition.getPropertyAsString(ACTIVITY_URI.resolve("#substitutes")));	
+		assertTrue(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#charset")));
+		assertEquals(CHARSET.resolve("#windows-1252"),
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#charset")));
+
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#forceCopy")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#concatenate")));
+		
+		// 4 - fileList2
+		portDefinition = scufl2Tools.portDefinitionFor(fileList2, profile);
+		assertNotNull("Could not find port definition for fileList2", portDefinition);
+		assertEquals(ACTIVITY_URI.resolve("#binary"),
+				portDefinition.getPropertyAsResourceURI(PORT_DEFINITION.resolve("#dataType")));
+		assertEquals(ACTIVITY_URI.resolve("#File"), 
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#substitutionType")));
+		assertEquals("another",
+				portDefinition.getPropertyAsString(ACTIVITY_URI.resolve("#substitutes")));	
+		assertTrue(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#charset")));
+		assertEquals(CHARSET.resolve("#windows-1252"),
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#charset")));
+
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#forceCopy")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#concatenate")));
+		
+		// 5 - stringReplace
+		portDefinition = scufl2Tools.portDefinitionFor(stringReplace, profile);
+		assertNotNull("Could not find port definition for stringReplace", portDefinition);
+		assertEquals(ACTIVITY_URI.resolve("#binary"),
+				portDefinition.getPropertyAsResourceURI(PORT_DEFINITION.resolve("#dataType")));
+		assertEquals(ACTIVITY_URI.resolve("#Parameter"), 
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#substitutionType")));
+		assertEquals("stringReplace",
+				portDefinition.getPropertyAsString(ACTIVITY_URI.resolve("#substitutes")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#charset")));
+		//assertEquals(CHARSET.resolve("#windows-1252"),
+				//portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#charset")));
+
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#forceCopy")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#concatenate")));
+
+
+		// 6 - stringReplaceWithDifferentPort
+		portDefinition = scufl2Tools.portDefinitionFor(stringReplaceWithDifferentPort, profile);
+		assertNotNull("Could not find port definition for stringReplaceWithDifferentPort", portDefinition);
+		assertEquals(ACTIVITY_URI.resolve("#binary"),
+				portDefinition.getPropertyAsResourceURI(PORT_DEFINITION.resolve("#dataType")));
+		assertEquals(ACTIVITY_URI.resolve("#Parameter"), 
+				portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#substitutionType")));
+		assertEquals("%%theString%%",
+				portDefinition.getPropertyAsString(ACTIVITY_URI.resolve("#substitutes")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#charset")));
+		//assertEquals(CHARSET.resolve("#windows-1252"),
+				//portDefinition.getPropertyAsResourceURI(ACTIVITY_URI.resolve("#charset")));
+
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#forceCopy")));
+		assertFalse(portDefinition.getProperties().containsKey(ACTIVITY_URI.resolve("#concatenate")));
 		
 		
 				
