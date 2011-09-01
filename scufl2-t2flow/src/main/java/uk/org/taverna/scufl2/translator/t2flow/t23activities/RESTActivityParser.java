9fa16ac7113c4f00b65814fc8d6b203c5d54c2f6
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-01 17:27:58 +0000

    SCUFL2-101 Check and parse 2.2 REST activity \n\nStill need to check POST/PUT and activity ports.
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13904 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/t23activities/RESTActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/t23activities/RESTActivityParser.java
@@ -1,17 +1,26 @@
 package uk.org.taverna.scufl2.translator.t2flow.t23activities;
 
+import static uk.org.taverna.scufl2.translator.t2flow.t23activities.RESTActivityParser.ACTIVITY_URI;
+import static uk.org.taverna.scufl2.translator.t2flow.t23activities.RESTActivityParser.HTTP_URI;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
+import uk.org.taverna.scufl2.api.common.Scufl2Tools;
 import uk.org.taverna.scufl2.api.common.URITools;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.api.port.InputActivityPort;
+import uk.org.taverna.scufl2.api.port.OutputActivityPort;
+import uk.org.taverna.scufl2.api.property.PropertyList;
+import uk.org.taverna.scufl2.api.property.PropertyLiteral;
 import uk.org.taverna.scufl2.api.property.PropertyResource;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.translator.t2flow.defaultactivities.AbstractActivityParser;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.ActivityInputs.Entry;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.RESTConfig;
 
@@ -39,7 +48,8 @@ public class RESTActivityParser extends AbstractActivityParser {
 	@Override
 	public boolean canHandlePlugin(URI activityURI) {
 		String activityUriStr = activityURI.toASCIIString();
-		return ( activityUriStr.startsWith(ravenURI.toASCIIString()) || activityUriStr.startsWith(ravenUIURI.toASCIIString()) ) 
+		return ( activityUriStr.startsWith(ravenURI.toASCIIString()) || 
+				 activityUriStr.startsWith(ravenUIURI.toASCIIString()) ) 
 				&& activityUriStr.endsWith(className);
 	}
 
@@ -65,8 +75,6 @@ public class RESTActivityParser extends AbstractActivityParser {
 	public Configuration parseConfiguration(T2FlowParser t2FlowParser,
 			ConfigBean configBean) throws ReaderException {
 
-		
-
 
 		RESTConfig restConfig = unmarshallConfig(t2FlowParser, configBean,
 					"xstream", RESTConfig.class);
@@ -79,12 +87,130 @@ public class RESTActivityParser extends AbstractActivityParser {
 					.getPropertyResource();
 			configResource.setTypeURI(ACTIVITY_URI.resolve("#Config"));
 
+			PropertyResource request = configResource.addPropertyAsNewResource(ACTIVITY_URI.resolve("#request"), 
+					ACTIVITY_URI.resolve("#Request"));
+						
+			URI method = HTTP_METHODS_URI.resolve("#" + restConfig.getHttpMethod().toUpperCase());
+			request.addPropertyReference(HTTP_URI.resolve("#mthd"), method);
 			
+			request.addPropertyAsString(ACTIVITY_URI.resolve("#absoluteURITemplate"), restConfig.getUrlSignature());
 
+			PropertyList headers = new PropertyList();
+			request.addProperty(HTTP_URI.resolve("#headers"), headers);
+			
+			if (restConfig.getAcceptsHeaderValue() != null && ! restConfig.getAcceptsHeaderValue().isEmpty()) {
+				PropertyResource accept = new PropertyResource();
+				accept.setTypeURI(HTTP_URI.resolve("#RequestHeader"));
+				accept.addPropertyAsString(HTTP_URI.resolve("#fieldName"), "Accept");
+				accept.addPropertyAsString(HTTP_URI.resolve("#fieldValue"), restConfig.getAcceptsHeaderValue());
+				//accept.addPropertyReference(HTTP_URI.resolve("#hdrName"), HTTP_METHODS_URI.resolve("#accept"));
+				headers.add(accept);
+			}
+			if (hasContent(method)) {
+				
+				if (restConfig.getContentTypeForUpdates() != null && ! restConfig.getContentTypeForUpdates().isEmpty()) {
+					PropertyResource contentType = new PropertyResource();
+					contentType.setTypeURI(HTTP_URI.resolve("#RequestHeader"));
+					contentType.addPropertyAsString(HTTP_URI.resolve("#fieldName"), "Content-Type");
+					contentType.addPropertyAsString(HTTP_URI.resolve("#fieldValue"), restConfig.getContentTypeForUpdates());
+					//accept.addPropertyReference(HTTP_URI.resolve("#hdrName"), HTTP_METHODS_URI.resolve("#content-type"));
+					headers.add(contentType);
+				} 
+				if (restConfig.isSendHTTPExpectRequestHeader()) {
+					PropertyResource expect = new PropertyResource();
+					expect.setTypeURI(HTTP_URI.resolve("#RequestHeader"));
+					expect.addPropertyAsString(HTTP_URI.resolve("#fieldName"), "Expect");
+					configResource.addProperty(ACTIVITY_URI.resolve("#use100Continue"), new PropertyLiteral(true));
+					//accept.addPropertyReference(HTTP_URI.resolve("#hdrName"), HTTP_METHODS_URI.resolve("#expect"));
+					headers.add(expect);
+				}
+			}
+			
+			if (restConfig.isShowRedirectionOutputPort()) {
+				configResource.addProperty(ACTIVITY_URI.resolve("#showRedirectionOutputPort"), new PropertyLiteral(true));
+			}
+			if (restConfig.isEscapeParameters() != null && ! restConfig.isEscapeParameters()) {
+				// Default: true
+				configResource.addProperty(ACTIVITY_URI.resolve("#escapeParameters"), new PropertyLiteral(false));
+			}
+			
+			// Ports
+			
+			if (restConfig.getActivityInputs() != null && restConfig.getActivityInputs().getEntry() != null) {
+				for (Entry entry : restConfig.getActivityInputs().getEntry()) {
+					String portName = entry.getString();
+					// Ignored, URL parameters have to be strings
+					//String className = entry.getJavaClass();
+					
+					InputActivityPort inputPort = new InputActivityPort(getParserState().getCurrentActivity(), portName);
+					inputPort.setDepth(0);
+					
+					
+					PropertyResource portConfig = configResource.addPropertyAsNewResource(
+							Scufl2Tools.PORT_DEFINITION.resolve("#inputPortDefinition"),
+							Scufl2Tools.PORT_DEFINITION.resolve("#InputPortDefinition"));
+
+					URI portUri = new URITools().relativeUriForBean(inputPort, configuration);
+					portConfig.addPropertyReference(Scufl2Tools.PORT_DEFINITION.resolve("#definesInputPort"), portUri);
+					portConfig.addPropertyReference(Scufl2Tools.PORT_DEFINITION.resolve("#dataType"),
+							PropertyLiteral.XSD_STRING);										
+				}
+			}
+			if (hasContent(method)) {
+				InputActivityPort inputPort = new InputActivityPort(getParserState().getCurrentActivity(), IN_BODY);
+				inputPort.setDepth(0);
+				
+				// FIXME: Is this really an #inputPortDefinition? It's not specified
+				// by the user - it's a consequence of the method choice. But if we don't do 
+				// this, then we'll have to specify the binary/string option elsewhere.
+				PropertyResource portConfig = configResource.addPropertyAsNewResource(
+						Scufl2Tools.PORT_DEFINITION.resolve("#inputPortDefinition"),
+						Scufl2Tools.PORT_DEFINITION.resolve("#InputPortDefinition"));
+
+				URI portUri = new URITools().relativeUriForBean(inputPort, configuration);
+				portConfig.addPropertyReference(Scufl2Tools.PORT_DEFINITION.resolve("#definesInputPort"), portUri);
+				
+				URI dataType = PropertyLiteral.XSD_STRING;
+				if (restConfig.getOutgoingDataFormat().equals("Binary")) {
+					dataType = Scufl2Tools.PORT_DEFINITION.resolve("#Binary");
+				}						
+				portConfig.addPropertyReference(Scufl2Tools.PORT_DEFINITION.resolve("#dataType"),
+						dataType);
+			}
+			
+			OutputActivityPort responseBody = new OutputActivityPort(getParserState().getCurrentActivity(), OUT_RESPONSE_BODY);
+			responseBody.setDepth(0);
+			OutputActivityPort status = new OutputActivityPort(getParserState().getCurrentActivity(), OUT_STATUS);
+			status.setDepth(0);
+			
+			if (restConfig.isShowRedirectionOutputPort()) {
+				OutputActivityPort redirection = new OutputActivityPort(getParserState().getCurrentActivity(), OUT_REDIRECTION);
+				redirection.setDepth(0);									
+			}
+			
+			
 			return configuration;
 		} finally {
 			getParserState().setCurrentConfiguration(null);
 		}
+	}
+	
+	private static final String IN_BODY = "inputBody";
+	private static final String OUT_RESPONSE_BODY = "responseBody";
+	private static final String OUT_STATUS = "status";
+	private static final String OUT_REDIRECTION = "redirection";
+
+	private boolean hasContent(URI method) {
+		if (! (method.resolve("#").equals(HTTP_METHODS_URI))) { 
+			throw new IllegalArgumentException("Only standard HTTP methods from " +
+					HTTP_METHODS_URI + " are supported");
+		}
+		String methodName = method.getFragment();		
+		if (Arrays.asList("GET", "HEAD", "DELETE", "CONNECT").contains(methodName)) {
+			return false;
+		}
+		// Most probably does have or could have content
+		return true;
 	}
 
 
