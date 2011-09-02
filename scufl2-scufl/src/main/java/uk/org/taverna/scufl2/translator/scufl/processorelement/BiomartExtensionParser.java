d80b071124c4c4c93ae9394b4bae06839a1fb048
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-02 16:57:33 +0000

    SCUFL to SCUFL2 translator
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13919 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ scufl2-scufl/src/main/java/uk/org/taverna/scufl2/translator/scufl/processorelement/BiomartExtensionParser.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package uk.org.taverna.scufl2.translator.scufl.processorelement;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.translator.scufl.ScuflExtensionParser;
+
+/**
+ * @author alanrw
+ *
+ */
+public class BiomartExtensionParser extends AbstractExtensionParser {
+
+	private static final String BIOMART_XSD = "../xsd/scufl-biomart.xsd";
+
+	/* (non-Javadoc)
+	 * @see uk.org.taverna.scufl2.translator.scufl.ScuflExtensionParser#canHandle(java.lang.Class)
+	 */
+	@Override
+	public boolean canHandle(Class c) {
+		return c.equals(uk.org.taverna.scufl2.xml.scufl.jaxb.BiomartType.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.org.taverna.scufl2.translator.scufl.ScuflExtensionParser#getAdditionalSchemas()
+	 */
+	@Override
+	public List<URI> getAdditionalSchemas() {
+		URL biomartXsd = getClass().getResource(BIOMART_XSD);
+		try {
+			return Arrays.asList(biomartXsd.toURI());
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException("Can't find Biomart schema "
+					+ biomartXsd);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.org.taverna.scufl2.translator.scufl.ScuflExtensionParser#parseScuflObject(java.lang.Object)
+	 */
+	@Override
+	public void parseScuflObject(Object o) {
+		System.err.println(this.getClass() + " is not yet implemented");
+	}
+
+}
