b419d9708caa8ff033bc85933cf1260bc02ea36a
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-02 15:25:13 +0000

    SCUFL2-104 Beanshell dependency and local worker name
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13918 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BeanshellActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BeanshellActivityParser.java
@@ -3,13 +3,16 @@ package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 import java.net.URI;
 
 import uk.org.taverna.scufl2.api.activity.Activity;
+import uk.org.taverna.scufl2.api.common.URITools;
 import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.api.property.PropertyResource;
 import uk.org.taverna.scufl2.translator.t2flow.ParserState;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ActivityPortDefinitionBean;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.BasicArtifact;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.BeanshellConfig;
+import uk.org.taverna.scufl2.xml.t2flow.jaxb.ClassLoaderSharing;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
 
 public class BeanshellActivityParser extends AbstractActivityParser {
@@ -27,6 +30,16 @@ public class BeanshellActivityParser extends AbstractActivityParser {
 	public static URI ACTIVITY_URI = URI
 			.create("http://ns.taverna.org.uk/2010/activity/beanshell");
 
+	public static URI LOCAL_WORKER_URI = URI
+			.create("http://ns.taverna.org.uk/2010/activity/localworker/");
+
+	
+	public static URI DEPENDENCY_URI = URI
+			.create("http://ns.taverna.org.uk/2010/activity/dependency");
+	
+	private static URITools uriTools = new URITools();
+
+	
 
 	@Override
 	public boolean canHandlePlugin(URI activityURI) {
@@ -52,7 +65,7 @@ public class BeanshellActivityParser extends AbstractActivityParser {
 	public Configuration parseConfiguration(T2FlowParser t2FlowParser,
 			ConfigBean configBean, ParserState parserState) throws ReaderException {
 		
-		// FIXME: Test with local workers
+	
 		BeanshellConfig beanshellConfig = unmarshallConfig(t2FlowParser,
 				configBean, "xstream", BeanshellConfig.class);
 
@@ -62,11 +75,50 @@ public class BeanshellActivityParser extends AbstractActivityParser {
 		PropertyResource configResource = configuration.getPropertyResource();
 		configResource.setTypeURI(ACTIVITY_URI.resolve("#Config"));
 		
+		if (beanshellConfig.getLocalworkerName() != null) {
+			URI localWorkerURI = LOCAL_WORKER_URI.resolve(uriTools.validFilename(beanshellConfig.getLocalworkerName()));
+			URI relation = ACTIVITY_URI.resolve("#derivedFrom");
+			// FIXME: As we can't read the annotation chain yet, we can't tell
+			// whether this local worker has been edited or not, and so 
+			// can't use #definedBy
+			configResource.addPropertyReference(relation, 
+					localWorkerURI);
+		}
+		
+		
 		String script = beanshellConfig.getScript();
 		configResource.addPropertyAsString(ACTIVITY_URI.resolve("#script"), script);
 
-		// TODO: Dependencies, activities, etc
 
+		ClassLoaderSharing classLoaderSharing = beanshellConfig.getClassLoaderSharing();
+		if (classLoaderSharing == ClassLoaderSharing.SYSTEM) {
+			configResource.addPropertyReference(DEPENDENCY_URI.resolve("#classLoader"), 
+					DEPENDENCY_URI.resolve("#SystemClassLoader"));				
+		} // default is WorkflowClassLoader
+ 		
+		if (beanshellConfig.getLocalDependencies() != null) {			
+			for (String localDep : beanshellConfig.getLocalDependencies().getString()) {
+				PropertyResource dependency = configResource.addPropertyAsNewResource(DEPENDENCY_URI.resolve("#dependency"), 
+						DEPENDENCY_URI.resolve("#LocalJarDependency"));
+				dependency.addPropertyAsString(DEPENDENCY_URI.resolve("#jarFile"), localDep);
+			}
+		}
+
+
+		/**
+		 * Note: Maven Dependencies are not supported by Taverna 3 - 
+		 * only here for t2flow->t2flow scenarios
+		 */
+		if (beanshellConfig.getArtifactDependencies() != null) {			
+			for (BasicArtifact mavenDep : beanshellConfig.getArtifactDependencies().getNetSfTavernaRavenRepositoryBasicArtifact()) {
+				PropertyResource dependency = configResource.addPropertyAsNewResource(DEPENDENCY_URI.resolve("#dependency"), 
+						DEPENDENCY_URI.resolve("#MavenDependency"));				
+				dependency.addPropertyAsString(DEPENDENCY_URI.resolve("#mavenGroupId"), mavenDep.getGroupId());
+				dependency.addPropertyAsString(DEPENDENCY_URI.resolve("#mavenArtifactId"), mavenDep.getArtifactId());
+				dependency.addPropertyAsString(DEPENDENCY_URI.resolve("#mavenVersion"), mavenDep.getVersion());
+			}
+		}
+		
 		Activity activity = parserState.getCurrentActivity();
 		activity.getInputPorts().clear();
 		activity.getOutputPorts().clear();
