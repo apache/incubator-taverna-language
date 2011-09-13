a325292ee16a7dd37be7ee3b0510403c0476f596
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-13 17:13:13 +0000

    media type recognizion for scufl1
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13993 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-scufl/src/main/java/uk/org/taverna/scufl2/translator/scufl/ScuflReader.java
+++ scufl2-scufl/src/main/java/uk/org/taverna/scufl2/translator/scufl/ScuflReader.java
@@ -6,6 +6,7 @@ package uk.org.taverna.scufl2.translator.scufl;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,13 +24,15 @@ import uk.org.taverna.scufl2.api.io.WorkflowBundleReader;
  */
 public class ScuflReader implements WorkflowBundleReader {
 	
+	public static final String APPLICATION_VND_TAVERNA_SCUFL_XML = "application/vnd.taverna.scufl+xml";
+
 	private Scufl2Tools scufl2Tools = new Scufl2Tools();
 
 	private ScuflParser parser;
 	
-	public static final String APPLICATION_XML = "application/xml";
-	public static final String TEXT_XML = "text/xml";
-	private static final Set<String> SCUFL_TYPES = new HashSet(Arrays.asList(new String[] {APPLICATION_XML, TEXT_XML}));
+//	public static final String APPLICATION_XML = "application/xml";
+	//public static final String TEXT_XML = "text/xml";
+	private static final Set<String> SCUFL_TYPES = new HashSet(Arrays.asList(new String[] {APPLICATION_VND_TAVERNA_SCUFL_XML}));
 
 	/* (non-Javadoc)
 	 * @see uk.org.taverna.scufl2.api.io.WorkflowBundleReader#getMediaTypes()
@@ -87,4 +90,22 @@ public class ScuflReader implements WorkflowBundleReader {
 		return parser;
 	}
 
+	@Override
+	public String guessMediaTypeForSignature(byte[] firstBytes) {
+
+		if (firstBytes.length < 100) { 
+			return null;
+		}
+		// FIXME: Does not deal with potential UTF-16 encoding
+		
+		// Latin 1 can deal with nasty bytes in binaries
+		Charset latin1 = Charset.forName("ISO-8859-1");
+		String asLatin1 = new String(firstBytes, latin1);
+		if (! asLatin1.contains("http://org.embl.ebi.escience/xscufl/0.1alpha")) { 
+			return null;
+		}
+		// Good enough - XML is hard to check on so few bytes		
+		return APPLICATION_VND_TAVERNA_SCUFL_XML;
+	}
+	
 }
