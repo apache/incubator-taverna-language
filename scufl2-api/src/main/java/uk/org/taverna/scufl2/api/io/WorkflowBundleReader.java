d33cfd9be57e8026ec152ad247990bb2d923b62c
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-13 17:11:46 +0000

    Guess media type
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@13989 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/io/WorkflowBundleReader.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/io/WorkflowBundleReader.java
@@ -10,20 +10,33 @@ import uk.org.taverna.scufl2.api.container.WorkflowBundle;
 /**
  * A reader for {@link WorkflowBundle}s.
  * 
- * Implementations specify workflow bundle formats (media types) that they can read.
+ * Implementations specify workflow bundle formats (media types/mime types) that
+ * they can read.
  */
 public interface WorkflowBundleReader {
 
 	/**
-	 * Returns the media types that this reader can handle.
+	 * Return the media types that this reader can read.
+	 * <p>
+	 * Returned media types are must be valid <code>mediaType</code> arguments
+	 * to {@link #readBundle(File, String)} and/or
+	 * {@link #readBundle(InputStream, String)} for this reader.
+	 * <p>
+	 * It is recommended, but not required, that the reader can also recognise
+	 * those media types from {@link #guessMediaTypeForSignature(byte[])}.
+	 * <p>
+	 * If the returned set is empty, the reader should should be able to
+	 * Recognise at least one media type from
+	 * {@link #guessMediaTypeForSignature(byte[])}.
 	 * 
-	 * @return the media types that this reader can handle
+	 * @return the media types that this reader can read, or an empty set if
+	 *         this reader can't read any bundle formats.
 	 */
 	public Set<String> getMediaTypes();
 
 	/**
-	 * Reads a file containing a workflow bundle in the specified media type and returns a
-	 * <code>WorkflowBundle</code>.
+	 * Read a file containing a workflow bundle in the specified media type and
+	 * return a <code>WorkflowBundle</code>.
 	 * 
 	 * @param bundleFile
 	 *            the file containing the workflow bundle
@@ -35,12 +48,12 @@ public interface WorkflowBundleReader {
 	 * @throws IOException
 	 *             if there is an error reading the file
 	 */
-	public WorkflowBundle readBundle(File bundleFile, String mediaType) throws ReaderException,
-	IOException;
+	public WorkflowBundle readBundle(File bundleFile, String mediaType)
+			throws ReaderException, IOException;
 
 	/**
-	 * Reads a stream containing a workflow bundle in the specified media type and returns a
-	 * <code>WorkflowBundle</code>.
+	 * Read a stream containing a workflow bundle in the specified media type
+	 * and return a <code>WorkflowBundle</code>.
 	 * 
 	 * @param inputStream
 	 *            the stream containing the workflow bundle
@@ -53,6 +66,26 @@ public interface WorkflowBundleReader {
 	 *             if there is an error reading from the stream
 	 */
 	public WorkflowBundle readBundle(InputStream inputStream, String mediaType)
-	throws ReaderException, IOException;
+			throws ReaderException, IOException;
+
+	/**
+	 * Attempt to guess the media type for a stream or file that starts with
+	 * these bytes.
+	 * <p>
+	 * Return <code>null</code> if ambiguous (more than one possibility) or
+	 * unknown.
+	 * <p>
+	 * Typically a WorkflowBundleReader should be able to recognise the same
+	 * types as those listed in {@link #getMediaTypes()}, but this is no
+	 * requirement. A WorkflowBundleReader could also recognise types not listed
+	 * in its {@link #getMediaTypes()}.
+	 * 
+	 * @param firstBytes
+	 *            The initial bytes, at least 512 bytes long unless the resource
+	 *            is smaller.
+	 * @return The recognised media type, or <code>null</code> if the bytes were
+	 *         ambiguous or unknown.
+	 */
+	public String guessMediaTypeForSignature(byte[] firstBytes);
 
 }
