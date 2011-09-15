c8fafcf5f0f493ac8dd4463ac1b741b33f6dfac2
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-15 16:32:33 +0000

    SCUFL2-108 Test against defaults
    Note that MAX_RETRIES default should be 0, not 1
    
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@14017 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultdispatchstack/RetryParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultdispatchstack/RetryParser.java
@@ -30,17 +30,15 @@ public class RetryParser extends AbstractActivityParser {
 				&& uriStr.endsWith(className);
 	}
 
-	/*
-	 * Defaults - ignored for now
+	/**
+	 * Default values - ignored for now
 	 */
-	/*
 	public static final class Defaults {
 		public static final int MAX_RETRIES = 1;
 		public static final int INITIAL_DELAY = 1000;
 		public static final int MAX_DELAY = 5000;
 		public static final int BACKOFF_FACTOR = 5000;
 	}
-	 */
 	
 	@Override
 	public URI mapT2flowRavenIdToScufl2URI(URI t2flowActivity) {
