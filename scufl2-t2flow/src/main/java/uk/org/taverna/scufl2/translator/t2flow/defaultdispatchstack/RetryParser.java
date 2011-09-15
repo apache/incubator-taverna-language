ee7b3f8b0d84961e39b65be96d1f824bf9989273
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-09-15 16:32:56 +0000

    SCUFL2-108 Don't include default values for Retry
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@14018 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultdispatchstack/RetryParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultdispatchstack/RetryParser.java
@@ -34,10 +34,10 @@ public class RetryParser extends AbstractActivityParser {
 	 * Default values - ignored for now
 	 */
 	public static final class Defaults {
-		public static final int MAX_RETRIES = 1;
+		public static final int MAX_RETRIES = 0;
 		public static final int INITIAL_DELAY = 1000;
 		public static final int MAX_DELAY = 5000;
-		public static final int BACKOFF_FACTOR = 5000;
+		public static final double BACKOFF_FACTOR = 1.0;
 	}
 	
 	@Override
@@ -59,19 +59,31 @@ public class RetryParser extends AbstractActivityParser {
 
 		PropertyResource resource = c.getPropertyResource();
 
-		BigInteger maxRetries = config.getMaxRetries();	
-		PropertyLiteral maxRetriesProp = new PropertyLiteral(maxRetries.longValue());
-		resource.addProperty(scufl2Uri.resolve("#maxRetries"), maxRetriesProp);
-	
-		PropertyLiteral initialDelayProp = new PropertyLiteral(config.getInitialDelay());
-		resource.addProperty(scufl2Uri.resolve("#initialDelay"), initialDelayProp);
-		
-		PropertyLiteral maxDelay = new PropertyLiteral(config.getMaxDelay());
-		resource.addProperty(scufl2Uri.resolve("#maxDelay"), maxDelay);
-		
-		PropertyLiteral backoffFactor = new PropertyLiteral(config.getBackoffFactor());
-		resource.addProperty(scufl2Uri.resolve("#backoffFactor"), backoffFactor);
-		
+		BigInteger maxRetries = config.getMaxRetries();
+		if (maxRetries.longValue() != Defaults.MAX_RETRIES || maxRetries.longValue() < 0) {
+			PropertyLiteral maxRetriesProp = new PropertyLiteral(maxRetries.longValue());
+			resource.addProperty(scufl2Uri.resolve("#maxRetries"), maxRetriesProp);
+		}
+
+		if (maxRetries.longValue() != 0) {
+			// Neither of these makes sense if retries are disabled
+
+			if (config.getInitialDelay() != Defaults.INITIAL_DELAY && config.getInitialDelay() > -1) {
+				PropertyLiteral initialDelayProp = new PropertyLiteral(config.getInitialDelay());
+				resource.addProperty(scufl2Uri.resolve("#initialDelay"), initialDelayProp);
+			}
+			
+			if (config.getMaxDelay() != Defaults.MAX_DELAY && config.getMaxDelay() > -1) {
+				PropertyLiteral maxDelay = new PropertyLiteral(config.getMaxDelay());
+				resource.addProperty(scufl2Uri.resolve("#maxDelay"), maxDelay);
+			}
+
+			double delta = Math.abs(config.getBackoffFactor() - Defaults.BACKOFF_FACTOR);			
+			if (config.getBackoffFactor() > 0 && delta > 1e-14) {
+				PropertyLiteral backoffFactor = new PropertyLiteral(config.getBackoffFactor());
+				resource.addProperty(scufl2Uri.resolve("#backoffFactor"), backoffFactor);
+			}
+		}
 		return c;
 	}
 }
