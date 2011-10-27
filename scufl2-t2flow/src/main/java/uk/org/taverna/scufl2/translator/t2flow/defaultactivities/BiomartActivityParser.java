aeceb4e5b00fec6d474f89321b65363e38882c42
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-10-27 16:27:04 +0000

    SCUFL2-49 Implemented parseConfiguration for biomart t2flow parser.
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@14188 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BiomartActivityParser.java
+++ scufl2-t2flow/src/main/java/uk/org/taverna/scufl2/translator/t2flow/defaultactivities/BiomartActivityParser.java
@@ -2,7 +2,12 @@ package uk.org.taverna.scufl2.translator.t2flow.defaultactivities;
 
 import java.net.URI;
 
+import org.w3c.dom.Element;
+
 import uk.org.taverna.scufl2.api.configurations.Configuration;
+import uk.org.taverna.scufl2.api.io.ReaderException;
+import uk.org.taverna.scufl2.api.property.PropertyLiteral;
+import uk.org.taverna.scufl2.api.property.PropertyResource;
 import uk.org.taverna.scufl2.translator.t2flow.ParserState;
 import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 import uk.org.taverna.scufl2.xml.t2flow.jaxb.ConfigBean;
@@ -14,7 +19,7 @@ public class BiomartActivityParser extends AbstractActivityParser {
 
 	private static String activityClassName = "net.sf.taverna.t2.activities.biomart.BiomartActivity";
 
-	public static URI scufl2Uri = URI
+	public static URI ACTIVITY_URI = URI
 			.create("http://ns.taverna.org.uk/2010/activity/biomart");
 
 	@Override
@@ -26,14 +31,22 @@ public class BiomartActivityParser extends AbstractActivityParser {
 
 	@Override
 	public URI mapT2flowRavenIdToScufl2URI(URI t2flowActivity) {
-		return scufl2Uri;
+		return ACTIVITY_URI;
 	}
 
 	@Override
 	public Configuration parseConfiguration(T2FlowParser t2FlowParser,
-			ConfigBean configBean, ParserState parserState) {
-		// TODO Auto-generated method stub
-		return null;
+			ConfigBean configBean, ParserState parserState) throws ReaderException {
+		Configuration configuration = new Configuration();
+		configuration.setParent(parserState.getCurrentProfile());
+
+		PropertyResource configResource = configuration.getPropertyResource();
+		configResource.setTypeURI(ACTIVITY_URI.resolve("#Config"));
+
+		PropertyLiteral literalXml = new PropertyLiteral((Element) configBean.getAny());
+		configResource.addProperty(ACTIVITY_URI.resolve("#martQuery"), literalXml);
+
+		return configuration;
 	}
 
 }
