e14ae982f6b9587545b6994a8e7d64f98822f454
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-12-21 04:37:33 +0000

    Port are not Configurable
    (what would that even mean?)
    
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/scufl2/trunk@14361 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/Port.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/Port.java
@@ -1,6 +1,5 @@
 package uk.org.taverna.scufl2.api.port;
 
-import uk.org.taverna.scufl2.api.common.Configurable;
 import uk.org.taverna.scufl2.api.common.Named;
 
 /**
@@ -9,5 +8,5 @@ import uk.org.taverna.scufl2.api.common.Named;
  * 
  * @author Alan R Williams
  */
-public interface Port extends Named, Configurable {
+public interface Port extends Named {
 }
