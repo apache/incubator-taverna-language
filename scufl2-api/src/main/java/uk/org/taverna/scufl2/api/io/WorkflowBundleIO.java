c26ef4948b0e52f48fd9a095e461f13ec3d69cde
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-01-30 09:21:44 +0000

    Organize imports

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/io/WorkflowBundleIO.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/io/WorkflowBundleIO.java
@@ -3,7 +3,6 @@ package uk.org.taverna.scufl2.api.io;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
