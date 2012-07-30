3e86614d88ce31c611822f25df13edbf3eefee74
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-07-30 15:56:11 +0000

    Improved Sender/ReceiverPort JavaDoc
    git-svn-id: https://taverna.googlecode.com/svn/scufl2/trunk@15083 bf327186-88b3-11dd-a302-d386e5130c1c

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/ReceiverPort.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/ReceiverPort.java
@@ -1,9 +1,15 @@
 package uk.org.taverna.scufl2.api.port;
 
+import uk.org.taverna.scufl2.api.core.DataLink;
+import uk.org.taverna.scufl2.api.core.Workflow;
+
 /**
- * A <code>Port</code> that receives data.
+ * A <code>Port</code> that receives data from a {@link DataLink} in a {@link Workflow}.
  * 
+ * @see SenderPort
+ * @see DataLink
  * @author Alan R Williams
+ * @author Stian Soiland-Reyes
  */
 public interface ReceiverPort extends Port {
 
--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/SenderPort.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/port/SenderPort.java
@@ -1,9 +1,15 @@
 package uk.org.taverna.scufl2.api.port;
 
+import uk.org.taverna.scufl2.api.core.DataLink;
+import uk.org.taverna.scufl2.api.core.Workflow;
+
 /**
- * A <code>Port</code> that sends data out.
+ * A <code>Port</code> that sends data out to a {@link DataLink} in a {@link Workflow}.
  * 
+ * @see DataLink
+ * @see Workflow
  * @author Alan R Williams
+ * @author Stian Soiland-Reyes
  */
 public interface SenderPort extends Port {
 
