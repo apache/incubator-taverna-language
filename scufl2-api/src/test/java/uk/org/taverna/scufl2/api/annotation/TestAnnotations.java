cf5273db1d1923939e886ba914e86db8c8c89d2c
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-09-26 11:10:21 +0100

    Removed irrelevant test (new tests needed!)

--- scufl2-api/src/test/java/uk/org/taverna/scufl2/api/annotation/TestAnnotations.java
+++ scufl2-api/src/test/java/uk/org/taverna/scufl2/api/annotation/TestAnnotations.java
@@ -13,17 +13,6 @@ import uk.org.taverna.scufl2.ucfpackage.UCFPackage.ResourceEntry;
 
 public class TestAnnotations {
 	
-	@Test
-	public void listAnnotations() throws Exception {
-		WorkflowBundle wfBundle = new WorkflowBundle();
-		UCFPackage resources = wfBundle.getResources();
-		assertTrue(resources.listResources(wfBundle.getAnnotationResourcesFolder()).isEmpty());
-		
-		resources.addResource("Dummy", wfBundle.getAnnotationResourcesFolder() + "test.txt", "text/plain");
-		Map<String, ResourceEntry> annotations = resources.listResources(wfBundle.getAnnotationResourcesFolder());
-		assertEquals(1, annotations.size());
-		assertEquals("test.txt", annotations.keySet().iterator().next());
-	}
 	
 	public void addAnnotation() {
 		WorkflowBundle wfBundle = new WorkflowBundle();
