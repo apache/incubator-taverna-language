4d7a51deb362a6a4977e7b4e43b28bcc0d1c74fe
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-10-29 16:59:16 +0000

    Just print out stuff..

--- scufl2-api/src/test/java/uk/org/taverna/scufl2/api/common/TestAbstractCloneable.java
+++ scufl2-api/src/test/java/uk/org/taverna/scufl2/api/common/TestAbstractCloneable.java
@@ -1,5 +1,23 @@
 package uk.org.taverna.scufl2.api.common;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.io.TestWorkflowBundleIO;
+
 public class TestAbstractCloneable {
-	
+	private WorkflowBundle wfBundle;
+
+	@Before
+	public void makeExampleWorkflow() {
+		wfBundle = new TestWorkflowBundleIO().makeWorkflowBundle();
+	}
+
+	@Test
+	public void megaClone() throws Exception {
+		AbstractCloneable clone = wfBundle.cloned();
+	}
 }
