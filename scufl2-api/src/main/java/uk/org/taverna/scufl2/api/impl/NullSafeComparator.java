3a06d6e3168a18cef4b48955e897bf2ea0a3a519
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-01-16 15:27:29 +0000

    Added compareObjects static method

--- scufl2-api/src/main/java/uk/org/taverna/scufl2/api/impl/NullSafeComparator.java
+++ scufl2-api/src/main/java/uk/org/taverna/scufl2/api/impl/NullSafeComparator.java
@@ -10,17 +10,21 @@ import java.util.Comparator;
  * <code>null</code>s as equal.
  * </p>
  * <p>
- * 
+ * The static method {@link #compareObjects(Object, Object)} can be used if such a comparison
+ * is needed without having to instantiate this class, for instance because it is used within
+ * a {@link Comparable#compareTo(Object)} or because the two arguments are not of an agreeable
+ * subclass &lt<T&gt; of {@link Comparable} (but still can be compared).
+ * </p>
  * 
  * @author Stian Soiland-Reyes
  * 
- * @param <T>
+ * @param <T> The common type of the objects to be compared.
  */
 public class NullSafeComparator<T extends Comparable<T>> implements
 		Comparator<T> {
 
-	@Override
-	public int compare(T a, T b) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static int compareObjects(Object a, Object b) {
 		if (a == null && b == null) {
 			return 0;
 		}
@@ -30,7 +34,12 @@ public class NullSafeComparator<T extends Comparable<T>> implements
 		if (a != null && b == null) {
 			return 1;
 		}
-		return a.compareTo(b);
+		return ((Comparable)a).compareTo(b);
+	}
+	
+	@Override
+	public int compare(T a, T b) {
+		return compareObjects(a, b);
 	}
 
 }
