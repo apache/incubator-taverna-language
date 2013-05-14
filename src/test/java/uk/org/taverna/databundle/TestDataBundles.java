f4e4fea925ff31bcdf4b19136bb85e463e9fc8bc
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-05-14 10:27:25 +0100

    Organize imports

--- src/test/java/uk/org/taverna/databundle/TestDataBundles.java
+++ src/test/java/uk/org/taverna/databundle/TestDataBundles.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
