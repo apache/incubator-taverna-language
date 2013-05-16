987305a34843e0fbdb69994ad8b78defa63642eb
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-05-16 17:58:06 +0100

    Also test a directory can't become a file

--- src/test/java/org/purl/wf4ever/robundle/fs/TestBundleFileSystem.java
+++ src/test/java/org/purl/wf4ever/robundle/fs/TestBundleFileSystem.java
@@ -31,13 +31,14 @@ public class TestBundleFileSystem {
     }
 
     /**
-     * Test that BundleFileSystem does not allow a ZIP file to also be a folder.
-     * See http://stackoverflow.com/questions/16588321/
+     * Test that BundleFileSystem does not allow a ZIP file to also become a
+     * directory. See http://stackoverflow.com/questions/16588321/ as Java 7'z
+     * ZIPFS normally allows this (!)
      * 
      * @throws Exception
      */
     @Test
-    public void directoryAndFile() throws Exception {
+    public void fileAndDirectory() throws Exception {
         Path folder = fs.getPath("folder");
         assertFalse(Files.exists(folder));
         Files.createFile(folder);
@@ -51,6 +52,7 @@ public class TestBundleFileSystem {
             fail("Should have thrown FileAlreadyExistsException");
         } catch (FileAlreadyExistsException ex) {
         }
+        assertFalse(Files.isDirectory(folder));
 
         try {
             Files.createDirectories(folder);
@@ -65,8 +67,49 @@ public class TestBundleFileSystem {
             fail("Should have thrown NotDirectoryException");
         } catch (NotDirectoryException ex) {
         }
+        assertFalse(Files.exists(child));
+
+        assertTrue(Files.isRegularFile(folder));
+        assertFalse(Files.isDirectory(folder));
+        assertFalse(Files.isDirectory(child.getParent()));
+        assertFalse(Files.isDirectory(fs.getPath("folder/")));
+    }
+
+    /**
+     * Test that BundleFileSystem does not allow a ZIP directory to also become
+     * a file. See http://stackoverflow.com/questions/16588321/ as Java 7'z
+     * ZIPFS normally allows this (!)
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void directoryAndFile() throws Exception {
+        Path folder = fs.getPath("folder/");
+        assertFalse(Files.exists(folder));
+
+        Files.createDirectory(folder);
+        assertTrue(Files.exists(folder));
+        assertFalse(Files.isRegularFile(folder));
+        assertTrue(Files.isDirectory(folder));
+
+        try {
+            Files.createDirectory(folder);
+            fail("Should have thrown FileAlreadyExistsException");
+        } catch (FileAlreadyExistsException ex) {
+        }
+
+        try {
+            Files.createFile(folder);
+            // Disable for now, just to see where this leads
+            fail("Should have thrown FileAlreadyExistsException");
+        } catch (FileAlreadyExistsException ex) {
+        }
+
+        Path child = folder.resolve("child");
+        Files.createFile(child);
 
         assertTrue(Files.isRegularFile(folder));
         assertFalse(Files.isDirectory(fs.getPath("folder/")));
     }
+
 }
