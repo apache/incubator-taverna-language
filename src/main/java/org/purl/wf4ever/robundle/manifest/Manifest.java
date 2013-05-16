36db5cadc712fc5a7049e912b512652bb62c2fa4
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-05-16 23:20:21 +0100

    populate() skeleton and Naive test of PathMetadata

--- src/main/java/org/purl/wf4ever/robundle/manifest/Manifest.java
+++ src/main/java/org/purl/wf4ever/robundle/manifest/Manifest.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.purl.wf4ever.robundle.Bundle;
+
 public class Manifest {
     URI id;
     List<Path> manifest;
@@ -16,6 +18,11 @@ public class Manifest {
     List<PathMetadata> aggregates;
     List<PathAnnotation> annotations;
     List<String> graph;
+    
+    public void populateFromBundle(Bundle bundle) {
+        // TODO Auto-generated method stub
+        
+    }
     
     
 }
