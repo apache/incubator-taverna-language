f62f10d6f005ebe471dda41e88b77bd7cb73bdec
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-05-17 00:31:55 +0100

    Test by URIs

--- src/test/java/org/purl/wf4ever/robundle/manifest/TestManifest.java
+++ src/test/java/org/purl/wf4ever/robundle/manifest/TestManifest.java
@@ -1,8 +1,9 @@
 package org.purl.wf4ever.robundle.manifest;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,7 +11,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,31 +20,39 @@ import org.purl.wf4ever.robundle.Bundles;
 
 public class TestManifest {
     @Test
-    public void populateFromZip() throws Exception {
-        GregorianCalendar before = new GregorianCalendar();
+    public void populateFromBundle() throws Exception {
         Bundle bundle = exampleBundle();
-        GregorianCalendar created = new GregorianCalendar();
 
         Path r = bundle.getRoot();
         URI base = r.toUri();
-        
+
         Manifest manifest = new Manifest();
         manifest.populateFromBundle(bundle);
-        GregorianCalendar populated = new GregorianCalendar();
 
-
-        List<Path> paths = new ArrayList<>();
+        List<String> uris = new ArrayList<>();
         for (PathMetadata s : manifest.aggregates) {
+            uris.add(s.file.toASCIIString());
             Path path = uri2path(base, s.file);
-            paths.add(path);
             assertNotNull(path.getParent());
-            assertEquals(path.getParent(), s.folder);
-            assertEquals(s.proxy.getScheme(), "urn:uuid");
-            UUID.fromString(s.proxy.getSchemeSpecificPart());
-            assertTrue(s.createdOn.after(before));
-            assertTrue(s.createdOn.before(created));
-            assertTrue(s.createdOn.before(populated));            
+            assertEquals(Manifest.withSlash(path.getParent()), s.folder);
+            if (s.file.equals(URI.create("f/nested/empty/"))) {
+                continue;
+                // Folder's don't need proxy and createdOn
+            }
+            System.out.println(s.file);
+            assertEquals("urn", s.proxy.getScheme());
+            UUID.fromString(s.proxy.getSchemeSpecificPart().replace("uuid:", ""));
+            assertEquals(s.createdOn, Files.getLastModifiedTime(path));
         }
+        assertFalse(uris.contains("mimetype"));
+        assertFalse(uris.contains("META-INF"));
+        assertTrue(uris.remove("hello.txt"));
+        assertTrue(uris.remove("f/file1.txt"));
+        assertTrue(uris.remove("f/file2.txt"));
+        assertTrue(uris.remove("f/file3.txt"));
+        assertTrue(uris.remove("f/nested/file1.txt"));
+        assertTrue(uris.remove("f/nested/empty/"));
+        assertTrue(uris.isEmpty());
     }
 
     private Path uri2path(URI base, URI uri) {
@@ -53,20 +61,24 @@ public class TestManifest {
     }
 
     private Bundle exampleBundle() throws IOException {
-        Bundle bundle = Bundles.createBundle();
-        Path r = bundle.getRoot();
-        Files.createFile(r.resolve("hello.txt"));
-        Path f = r.resolve("f");
-        Files.createDirectory(f);
-        Files.createFile(f.resolve("file3.txt"));
-        Files.createFile(f.resolve("file2.txt"));
-        Files.createFile(f.resolve("file1.txt"));
+        Path source;
+        try (Bundle bundle = Bundles.createBundle()) {
+            source = bundle.getSource();
+            Path r = bundle.getRoot();
+            Files.createFile(r.resolve("hello.txt"));
+            Path f = r.resolve("f");
+            Files.createDirectory(f);
+            Files.createFile(f.resolve("file3.txt"));
+            Files.createFile(f.resolve("file2.txt"));
+            Files.createFile(f.resolve("file1.txt"));
 
-        Path nested = f.resolve("nested");
-        Files.createDirectory(nested);
-        Files.createFile(nested.resolve("file1.txt"));
+            Path nested = f.resolve("nested");
+            Files.createDirectory(nested);
+            Files.createFile(nested.resolve("file1.txt"));
 
-        Files.createDirectory(f.resolve("empty"));
-        return bundle;
+            Files.createDirectory(nested.resolve("empty"));
+            bundle.setDeleteOnClose(false);
+        }
+        return Bundles.openBundle(source);
     }
 }
