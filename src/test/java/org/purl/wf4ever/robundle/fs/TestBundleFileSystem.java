dbad09bf2b54d4491e03dd2726d3ad99eb6ba404
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-05-30 16:07:05 +0100

    Those imports..

--- src/test/java/org/purl/wf4ever/robundle/fs/TestBundleFileSystem.java
+++ src/test/java/org/purl/wf4ever/robundle/fs/TestBundleFileSystem.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 
 import org.junit.After;
