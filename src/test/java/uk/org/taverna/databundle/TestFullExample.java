49b949b10572ae82105f3b3b2da1095996363f12
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-05-31 12:00:20 +0100

    Creating a full example workflow run RO

---
+++ src/test/java/uk/org/taverna/databundle/TestFullExample.java
@@ -0,0 +1,105 @@
+package uk.org.taverna.databundle;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Desktop;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.purl.wf4ever.robundle.Bundle;
+
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+
+public class TestFullExample {
+    
+    private static String RUN = "/full-example/ebi-wfrun-2013-05-31/";
+    
+    private static WorkflowBundleIO wfBundleIO = new WorkflowBundleIO();
+    
+    
+	@Test
+	public void writeExample() throws Exception {
+		// Create a new (temporary) data bundle
+		Bundle dataBundle = DataBundles.createBundle();
+
+		// The run as currently saved to a folder by prov-taverna 1.10
+		URL runResource = getClass().getResource(RUN);
+		Path runPath = Paths.get(runResource.toURI());
+		
+		assertTrue(Files.isDirectory(runPath));
+		
+		// In order to preserve existing file extensions we copy as files
+		// rather than using the higher-level methods like
+		// DataBundles.setStringValue()
+		
+		// Inputs
+		Path inputs = DataBundles.getInputs(dataBundle);
+		for (String filename : Arrays.asList("email.txt", "sequence.txt")) {
+		    Files.copy(runPath.resolve(filename), 
+		               inputs.resolve(filename));
+		}
+		
+		// Outputs
+        Path outputs = DataBundles.getInputs(dataBundle);
+        for (String filename : Arrays.asList("getResult_3_output_output.xml",
+                "getResult_output_output.octet-stream",
+                "Graphical_output.png",
+                "Workflow16_getStatus_output_status.txt")) {
+            Files.copy(runPath.resolve(filename), 
+                    outputs.resolve(filename));
+        }
+		
+        // Provenance
+        Files.copy(runPath.resolve("workflowrun.prov.ttl"), 
+                DataBundles.getWorkflowRunProvenance(dataBundle));
+
+        // Workflow
+        WorkflowBundle wfBundle = wfBundleIO.readBundle(runPath.resolveSibling("ebi_interproscan_newservices_900329.t2flow").toFile(), null);
+		DataBundles.setWorkflowBundle(dataBundle, wfBundle);
+        
+        
+        // Intermediate values
+		
+		
+		
+		// Saving a data bundle:
+		Path zip = Files.createTempFile("databundle", ".zip");
+		DataBundles.closeAndSaveBundle(dataBundle, zip);
+		// NOTE: From now dataBundle and its Path's are CLOSED 
+		// and can no longer be accessed
+		
+		
+		System.out.println("Saved to " + zip);
+		if (Desktop.isDesktopSupported()) {
+			// Open ZIP file for browsing
+			Desktop.getDesktop().open(zip.toFile());
+		}
+		
+		// Loading a data bundle back from disk
+		try (Bundle dataBundle2 = DataBundles.openBundle(zip)) {
+			assertEquals(zip, dataBundle2.getSource());
+			Path loadedInputs = DataBundles.getInputs(dataBundle2);
+			
+			for (Path port : DataBundles.getPorts(loadedInputs).values()) {
+				if (DataBundles.isValue(port)) {
+					System.out.print("Value " + port + ": ");
+					System.out.println(DataBundles.getStringValue(port));
+				} else if (DataBundles.isList(port)) {
+					System.out.print("List " + port + ": ");
+					for (Path item : DataBundles.getList(port)) {
+						// We'll assume depth 1 here
+						System.out.print(DataBundles.getStringValue(item));
+						System.out.print(", ");
+					}
+					System.out.println();
+				}				
+			}			
+		}				
+	}
+}
