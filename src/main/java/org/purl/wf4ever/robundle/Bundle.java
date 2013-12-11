7b6f79fc5c6e66d9f0d2a55c587cbe32522ba85f
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-12-11 13:59:11 +0000

    Keep (load/generate if needed) Manifest. Saved on close()
    TODO: Updating manifest for every operation!

--- src/main/java/org/purl/wf4ever/robundle/Bundle.java
+++ src/main/java/org/purl/wf4ever/robundle/Bundle.java
@@ -2,16 +2,21 @@ package org.purl.wf4ever.robundle;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.apache.jena.riot.RiotException;
 import org.purl.wf4ever.robundle.fs.BundleFileSystem;
+import org.purl.wf4ever.robundle.manifest.Manifest;
+import org.purl.wf4ever.robundle.manifest.RDFToManifest;
 
 public class Bundle implements Closeable {
 
     private boolean deleteOnClose;
     private final Path root;
+    private Manifest manifest;
 
     public Bundle(Path root, boolean deleteOnClose) {
         this.root = root;
@@ -27,6 +32,8 @@ public class Bundle implements Closeable {
         if (! getFileSystem().isOpen()) {
             return;
         }
+        getManifest().writeAsJsonLD();
+        
         getFileSystem().close();
         if (deleteOnClose) {
             Files.deleteIfExists(getSource());
@@ -53,5 +60,32 @@ public class Bundle implements Closeable {
     public void setDeleteOnClose(boolean deleteOnClose) {
         this.deleteOnClose = deleteOnClose;
     }
+
+    public Manifest getManifest() throws IOException {
+        if (manifest == null) {
+            synchronized(this) {
+                if (manifest == null) {
+                    manifest = readOrPopulateManifest();
+                }
+            }
+        }
+        return manifest;
+    }
+
+    protected Manifest readOrPopulateManifest() throws IOException {
+        Manifest newManifest = new Manifest(this);                    
+        Path manifestPath = Bundles.getManifestPath(this);
+        if (Files.exists(manifestPath)) { 
+            try (InputStream manifestStream = Files.newInputStream(manifestPath)) { 
+                new RDFToManifest().readTo(manifestStream, newManifest);                
+            }
+            // TODO: Also support reading manifest.rdf?
+        } else {
+            // Fallback (might be a fresh or 3rd party bundle), populate from zip content
+            newManifest.populateFromBundle();
+        }
+        return newManifest;
+    }
+
 
 }
