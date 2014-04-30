6b2993ba745e894fe7f29460b9d9eb008194fe19
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-30 11:00:18 +0100

    Test media guess on t2flow with text/turtle annotations
    .. should NOT be matched as text/turtle or application/rdf+xml

---
+++ src/test/java/org/purl/wf4ever/wfdesc/scufl/TestWfdescReader.java
@@ -0,0 +1,44 @@
+package org.purl.wf4ever.wfdesc.scufl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+
+public class TestWfdescReader {
+
+	WorkflowBundleIO io = new WorkflowBundleIO();
+	
+	@Test
+	public void guessType() throws Exception {		
+		assertEquals("application/vnd.taverna.t2flow+xml", 
+				guessMediaTypeForResource("/helloworld.t2flow"));
+		assertEquals("application/vnd.taverna.t2flow+xml", 
+				guessMediaTypeForResource("/rdf-in-example-annotation.t2flow"));
+
+		assertEquals("text/vnd.wf4ever.wfdesc+turtle", 
+				guessMediaTypeForResource("/helloworld.wfdesc.ttl"));
+		
+		assertNull( 
+				guessMediaTypeForResource("/allTypes.links.sparql.json"));
+	}
+	
+	private String guessMediaTypeForResource(String resource) throws IOException {
+		byte[] firstBytes = new byte[1024];
+		InputStream s = getClass().getResourceAsStream(resource);
+		assertNotNull("Could not find " + resource, s);
+		try {
+			s.read(firstBytes);
+		} finally { 
+			s.close();
+		}
+		return io.guessMediaTypeForSignature(firstBytes);
+	}
+	
+}
