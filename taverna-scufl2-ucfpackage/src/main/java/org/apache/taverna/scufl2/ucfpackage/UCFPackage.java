5b185aca0ae57a11e27d1b0467303e359855981d
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-06-10 15:10:23 +0100

    getResourceEntry: ensure bundlePath exists

--- taverna-scufl2-ucfpackage/src/main/java/org/apache/taverna/scufl2/ucfpackage/UCFPackage.java
+++ taverna-scufl2-ucfpackage/src/main/java/org/apache/taverna/scufl2/ucfpackage/UCFPackage.java
@@ -588,7 +588,10 @@ public class UCFPackage implements Cloneable {
 
 	public ResourceEntry getResourceEntry(String path) {
 	    Path bundlePath = bundle.getRoot().resolve(path);
-		return new ResourceEntry(bundlePath);
+	    if (Files.exists(bundlePath)) {
+	    	return new ResourceEntry(bundlePath);
+	    }
+	    return null;
 	}
 
 	@SuppressWarnings("rawtypes")
