541f282f07322d32d0a49c9f959c831f432aac4a
Alan R Williams <alan.r.williams@manchester.ac.uk>
2015-01-30 16:51:48 +0000

    Added taverna-baclava-language

---
+++ taverna-baclava-language/src/main/java/org/apache/taverna/baclava/BaclavaWriter.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.apache.taverna.baclava;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+import java.io.Writer;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+ * @author alanrw
+ *
+ */
+public class BaclavaWriter {
+
+	public static void writeBaclava(DataThingMapType d, Writer w) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty("jaxb.formatted.output", true);
+		
+		ObjectFactory of = new ObjectFactory();
+		marshaller.marshal(of.createDataThingMap(d), w);
+	}
+
+}
