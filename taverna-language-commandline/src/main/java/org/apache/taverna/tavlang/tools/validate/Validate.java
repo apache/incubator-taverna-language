90551c0b6e179bd247cf4b2c84869095da205b46
Menaka Madushanka <menaka12350@gmail.com>
2015-06-24 19:19:14 +0530

    Validation added

--- taverna-language-commandline/src/main/java/org/apache/taverna/tavlang/tools/validate/Validate.java
+++ taverna-language-commandline/src/main/java/org/apache/taverna/tavlang/tools/validate/Validate.java
@@ -19,20 +19,110 @@ package org.apache.taverna.tavlang.tools.validate;
  * under the License.
  */
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.taverna.scufl2.api.container.WorkflowBundle;
-import org.apache.taverna.scufl2.validation.ValidationReport;
-import org.apache.taverna.scufl2.validation.Validator;
-import org.apache.taverna.scufl2.validation.structural.StructuralValidationListener;
-import org.apache.taverna.scufl2.validation.structural.StructuralValidator;
+import org.apache.taverna.scufl2.api.io.ReaderException;
+import org.apache.taverna.scufl2.api.io.WorkflowBundleIO;
+import org.apache.taverna.scufl2.validation.correctness.CorrectnessValidationListener;
+import org.apache.taverna.scufl2.validation.correctness.CorrectnessValidator;
+import org.apache.taverna.scufl2.validation.correctness.ReportCorrectnessValidationListener;
 
-
+/*
+ * Takes .wfbundle/ .t2flow as the input
+ * */
 public class Validate {
 
-	public Validate(WorkflowBundle wfb){
-		System.out.println("Validation started...");
-		StructuralValidator v = new StructuralValidator();
-		StructuralValidationListener l = v.validate(wfb);
-		ValidationReport r;
+	private String logfile;
+	private String finalrep = "";
+
+	public Validate(List<String> files, String file) {
+		this.logfile = file;
+		for (String f : files) {
+			File wfile = new File(f);
+			read(wfile);
+		}
 	}
-	
+
+	public void read(File file) {
+		WorkflowBundleIO io = new WorkflowBundleIO();
+		try {
+			this.finalrep += validate(io.readBundle(file, null), file.getName());
+			if (this.logfile != null) {
+				saveToLog(finalrep);
+			}
+		} catch (ReaderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+	}
+
+	public String validate(WorkflowBundle wfb, String file) {
+		System.out.println("Validation started...");
+		StringBuilder report2 = new StringBuilder();
+		CorrectnessValidationListener r = new ReportCorrectnessValidationListener();
+		CorrectnessValidator v = new CorrectnessValidator();
+		r = v.validate(wfb);
+		String report = r.toString().replace(
+				"ReportCorrectnessValidationListener [", "");
+
+		String[] sections = report.split(", ");
+		HashMap<String, String> map = new HashMap<>();
+
+		System.out.println("Validation completed.......");
+
+		if (!r.detectedProblems()){
+			report2.append("The workflow " + file + " has no errors. \n\n");
+		}
+			
+		
+//		System.out.println("The validation report for " + file);
+		report2.append("The validation report for " + file + "......\n");
+		report2.append("-------------------------------------------------------------------------------- \n");
+		for (int i = 0; i < sections.length; i++) {
+			String line = "-->"+sections[i].split("=")[0].replace("()", "").replace(
+					"get", "")
+					+ ":- ";
+			report2.append(line);
+			String line2 =  sections[i].split("=")[1].replace("[", "").replace("]","");
+			if(line2.equals("")) report2.append("null \n\n");
+			else report2.append("\t").append(line2).append("\n");
+			
+		
+		}
+		report2.append("--------------------------------------------------------------------------------- \n\n");
+		
+		if(r.detectedProblems())
+			System.out.println(report2.toString());
+		
+		return report2.toString();
+
+	}
+
+	public void saveToLog(String s) {
+		File logFile = new File(this.logfile);
+		FileWriter logwriter;
+		try {
+			logwriter = new FileWriter(logFile);
+			BufferedWriter blw = new BufferedWriter(logwriter);
+			blw.write(s);
+			blw.close();
+			logwriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
