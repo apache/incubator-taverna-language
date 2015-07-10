1d574447abf55c9fae63f59710a8207523e176a5
Menaka Madushanka <menaka12350@gmail.com>
2015-07-10 22:50:12 +0530

    Tools test conplete

--- taverna-language-commandline/src/test/java/org/apache/taverna/tavlang/test/TestTools.java
+++ taverna-language-commandline/src/test/java/org/apache/taverna/tavlang/test/TestTools.java
@@ -21,16 +21,60 @@ package org.apache.taverna.tavlang.test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
+import org.apache.taverna.tavlang.tools.Tools.ConvertionTools;
 import org.junit.Test;
+import org.junit.*;
+
+/*
+ * Test cases for the Tools Class.
+ * Input :- The output file format
+ * Output:- The relevant writer media type for that file format
+ * 
+ * */
 
 public class TestTools {
 
+	private ConvertionTools tools;
+	private HashMap<String, String> map = new HashMap<>();
 	
+	 
+	@Before
+	public void setMap(){
+		this.map.put("wfdesc", "text/vnd.wf4ever.wfdesc+turtle");
+		this.map.put("iwir", "application/vnd.shiwa.iwir+xml");
+		this.map.put("json", "application/ld+json");
+		this.map.put("wfbundle", "application/vnd.taverna.scufl2.workflow-bundle");
+		this.map.put("robundle", null);
+	}
 	
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testWfdesc() {
+		assertEquals(map.get("wfdesc"), ConvertionTools.valueOf("wfdesc").getMediaType(tools));
+	}
+
+	@Test
+	public void testIwir() {
+		assertEquals(map.get("iwir"), ConvertionTools.valueOf("iwir").getMediaType(tools));
+	}
+
+	@Test
+	public void testJson() {
+		assertEquals(map.get("json"), ConvertionTools.valueOf("json").getMediaType(tools));
+	}
+
+
+	@Test
+	public void testWfbundle() {
+		assertEquals(map.get("wfbundle"), ConvertionTools.valueOf("wfbundle").getMediaType(tools));
+	}
+	
+	
+	@Test
+	public void testRobundle() {
+		assertEquals(map.get("robundle"), ConvertionTools.valueOf("robundle").getMediaType(tools));
 	}
 
 }
