6c6dfa6c70797299d2a762da66f7b2abf9753787
Menaka Madushanka <menaka12350@gmail.com>
2015-07-24 03:54:35 +0530

    Validation report class

---
+++ taverna-robundle/src/main/java/org/apache/taverna/robundle/validator/ValidationReport.java
@@ -0,0 +1,38 @@
+package org.apache.taverna.robundle.validator;
+
+import java.util.ArrayList;
+
+public class ValidationReport {
+	
+	private ArrayList<String> errorList;
+	
+	private ArrayList<String> infoWarnings;
+	
+	private ArrayList<String> warnings;
+
+	public ArrayList<String> getErrorList() {
+		return errorList;
+	}
+
+	public void setErrorList(ArrayList<String> errorList) {
+		this.errorList = errorList;
+	}
+
+	public ArrayList<String> getInfoWarnings() {
+		return infoWarnings;
+	}
+
+	public void setInfoWarnings(ArrayList<String> infoWarnings) {
+		this.infoWarnings = infoWarnings;
+	}
+
+	public ArrayList<String> getWarnings() {
+		return warnings;
+	}
+
+	public void setWarnings(ArrayList<String> warnings) {
+		this.warnings = warnings;
+	}
+	
+
+}
