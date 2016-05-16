d40623b190aedeefd8c1dd0142aa4ac9a7e8f3fb
Stian Soiland-Reyes <stain@apache.org>
2016-05-16 11:02:53 +0100

    newer Jackson

--- taverna-scufl2-wfdesc/src/main/java/org/apache/taverna/scufl2/wfdesc/WfdescAgent.java
+++ taverna-scufl2-wfdesc/src/main/java/org/apache/taverna/scufl2/wfdesc/WfdescAgent.java
@@ -25,11 +25,10 @@ package org.apache.taverna.scufl2.wfdesc;
 import java.io.IOException;
 import java.net.URI;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
