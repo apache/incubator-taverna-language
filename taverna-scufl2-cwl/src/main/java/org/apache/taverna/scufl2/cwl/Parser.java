252e3d8b11914b16a8212ef86d1cf435017209fe
Majdi Haouech <m.haouech@criteo.com>
2018-05-21 15:29:02 +0200

    Add helper functions

--- taverna-scufl2-cwl/src/main/java/org/apache/taverna/scufl2/cwl/Parser.java
+++ taverna-scufl2-cwl/src/main/java/org/apache/taverna/scufl2/cwl/Parser.java
@@ -6,6 +6,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+
+class InputField {
+
+    public String key;
+    public String type;
+    public int position;
+    public String prefix;
+
+    public InputField(String _key) {
+        key = _key;
+        type = "";
+        position = -1;
+        prefix = "";
+    }
+
+    public InputField(String _key, String _type) {
+        key = _key;
+        type = _type;
+        position = -1;
+        prefix = "";
+    }
+
+    public InputField(String _key, String _type, int pos) {
+        key = _key;
+        type = _type;
+        position = pos;
+        prefix = "";
+    }
+
+    public InputField(String _key, String _type, int pos, String _prefix) {
+        key = _key;
+        type = _type;
+        position = pos;
+        prefix = _prefix;
+    }
+}
+
+
 public class Parser {
 
     private String yamlLine;
@@ -37,5 +75,46 @@ public class Parser {
         } catch (IOException e) {
             System.err.println("Parser init error: " + e );
         }
+    }
+
+
+
+
+    private int getNextLineIndex(int index) {
+        index++;
+
+        while(yamlFile.get(index).equals("")) {
+            index++;
+        }
+
+        return index;
+    }
+
+    public static int getDepth(String line) {
+        int count = 0;
+        int idx = 0;
+        while(idx < line.length()) {
+            if(line.charAt(idx) != ' ') {
+                break;
+            }
+            count++;
+            idx++;
+        }
+        assert count % 2 == 0;
+        return count / 2;
+    }
+
+    public static String getKeyFromLine(String line) {
+        int commaIndex = line.indexOf(':');
+        assert commaIndex != -1;
+
+        return line.substring(0, commaIndex).trim();
+    }
+
+    public static String getValueFromLine(String line) {
+        int commaIndex = line.indexOf(':');
+        assert commaIndex != -1;
+
+        return line.substring(commaIndex + 1).trim();
     }
 }
